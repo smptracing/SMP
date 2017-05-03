a5167a18050e339f71de765c9bc29224da22204d
Johan Ustua <jeus1609@gmail.com>
2017-05-03 09:48:25 -0500

    modificando header

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -47,20 +47,14 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="" class="site_title"><i class="fa fa-paw"></i> <span>SEMOPIP</span></a>
+              <a href="" class="site_title"><i class="fa fa-users"></i> <span>SEMOPIP</span></a>
             </div>
 
             <div class="clearfix"></div>
 
             <!-- menu profile quick info -->
             <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="<?php echo base_url();?>assets/images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Bienvenidos</span>
-                <h2>Seguimiento y Monitoreo de PIP</h2>
-              </div>
+              
             </div>
             <!-- /menu profile quick info -->
 
