9453429c26a3afb121c5c7de72a1a214faa47f21
Johan Ustua <jeus1609@gmail.com>
2017-05-04 08:18:07 -0500

    Revert  michael
    This reverts commit 0aca2a593aae2bcabcfb689b7f5edde5231b6514.
    
    # Conflicts:
    #	application/controllers/MSectorEntidadSpu.php

--- application/controllers/MSectorEntidadSpu.php
+++ application/controllers/MSectorEntidadSpu.php
@@ -19,6 +19,7 @@ class MSectorEntidadSpu extends CI_Controller
     /*fin pagina principal de la vista */
 
     /* Sector*/
+<<<<<<< HEAD
     public function GetSector()
     {
         if ($this->input->is_ajax_request()) {
@@ -60,6 +61,56 @@ class MSectorEntidadSpu extends CI_Controller
         }
 
     }
+=======
+	 function GetSector()
+	{
+		if ($this->input->is_ajax_request()) 
+		{
+		$datos=$this->Model_Sector->GetSector();
+		echo json_encode($datos);
+		}
+		else
+		{
+			show_404();
+		}
+	}
+	 function AddSector()
+	 {
+	    if ($this->input->is_ajax_request()) 
+	    {
+	      $txt_NombreSector =$this->input->post("txt_NombreSector");
+	      if($this->Model_Sector->AddSector($txt_NombreSector) == false)
+	        echo "Se añadio un  sector";
+	      else
+	        echo "No se añadio  un sector";  
+		 }
+		 else
+		  {
+		      show_404();
+		   }
+
+ 	 }
+ 	 function UpdateSector()
+	 {
+	    if ($this->input->is_ajax_request()) 
+	    {
+		      $txt_IdModificar =$this->input->post("txt_IdModificar");
+		      $txt_NombreSectorM =$this->input->post("txt_NombreSectorM");
+		      $hola="hola";
+		      if($this->Model_Sector->UpdateSector($txt_IdModificar,$txt_NombreSectorM) == false)
+		        echo "Se actualizo correctamente";
+		      else
+		        echo "No Se actualizo correctamente"; 
+	    }
+	    else
+	    {
+	      	show_404();
+	    }
+	    
+		
+
+ 	 }
+>>>>>>> parent of 0aca2a5... se modififico vista y controlador js
     /*fin sector*/
 
     /* Entidad*/
