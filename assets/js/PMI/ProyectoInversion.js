754d15f57b8a47023068f0881f4c62a543c9e153
Johan <jeus1609@gmail.com>
2017-05-16 12:26:38 -0500

    SUbiendo cambios Michael
    Agregando modificaciones

--- assets/js/PMI/ProyectoInversion.js
+++ assets/js/PMI/ProyectoInversion.js
@@ -68,20 +68,51 @@
                                     {"data":"devengado_ac_pi"},
                                     {"data":"fecha_registro_pi"},
                                     {"data":"fecha_viabilidad_pi"},
-                                    {"data":"nombre_ue"},
-                                    {"data":"nombre_naturaleza_inv"},
-                                    {"data":"nombre_tipologia_inv"},
-                                    {"data":"nombre_tipo_inversion"},
-                                    {"data":"codigo_grup_funcional"},
-                                    {"data":"nombre_nivel_gob"},
-                                    {"data":"nombre_meta_pres"},
-                                    {"data":"nombre_programa_pres"},
-                                    {"defaultContent":"<button type='button' class='editar btn btn-primary btn-xs' data-toggle='modal' data-target='#VentanaModificarFuncion'><i class='ace-icon fa fa-pencil bigger-120'></i></button><button type='button' class='eliminar btn btn-danger btn-xs' data-toggle='modal' data-target='#'><i class='fa fa-trash-o'></i></button>"}
+                                    {"defaultContent":"<button type='button' class='editar btn btn-primary btn-xs' data-toggle='modal' data-target='#'><i class='ace-icon fa fa-pencil  bigger-120'></i></button><button type='button' class='VerProyecto btn btn-success btn-xs' data-toggle='modal' data-target='#VerDetalleProyectoInversion'><i class='ace-icon fa fa-eye bigger-120'></i></button>"}
                                 ],
 
                                 "language":idioma_espanol
                     });              
-                        			   	
+                 ListaProyectoInversionData("#table-ProyectoInversion",table);  //obtener data de funcion para agregar  AGREGAR                             
+                }
+
+                /*fin listar proyecto de inversion*/
+                var ListaProyectoInversionData=function(tbody,table){
+                       $(tbody).on("click","button.VerProyecto",function(){
+                        var data=table.row( $(this).parents("tr")).data();
+                        var Id_ProyectoInver=data.id_pi;
+                        MostrarDetalleProyecto(Id_ProyectoInver);
+                        /*var txt_codigofuncionM=$('#txt_codigofuncionM').val(data.codigo_funcion);
+                        var txt_nombrefuncionM=$('#txt_nombrefuncionM').val(data.nombre_funcion);*/
+
+                    });
+                }
+                var MostrarDetalleProyecto=function(Id_ProyectoInver){
+                    event.preventDefault(); 
+                    html="";
+                    $("table-detalleProyectoInversion").html(html);
+                    $.ajax({
+                        "url":base_url +"index.php/ProyectoInversion/BuscarProyectoInversion",
+                        type:"POST",
+                        data:{Id_ProyectoInver:Id_ProyectoInver},
+                        success:function(respuesta){
+                         var registros = eval(respuesta);
+                            html+="<thead> <tr> <th class='active'>DATOS DEL PROYECTOS DE INVERSIÓN</th>  <th class='active' colspan='5'>DETALLE </th> </tr></thead>"
+                            for (var i = 0; i <registros.length;i++) {
+                              html +="<tbody> <tr><th class='success'>CÓDIGO ÚNICO</th><th  colspan='5'>"+registros[i]["codigo_unico_pi"]+"</th></tr> <tr><th class='success'>PROYECTO DE INVERSIÓN</th><th  colspan='5'>"+registros[i]["nombre_pi"]+"</th></tr>";    
+                              html +="<tr><th class='success'>COSTO</th><th  colspan='5'>"+registros[i]["costo_pi"]+"</th></tr> <tr><th class='success'>DEVENGADO</th><th  colspan='5'>"+registros[i]["devengado_ac_pi"]+"</th></tr>";
+                              html +="<tr><th class='success'>FECHA  DE REGISTRO</th><th  colspan='5'>"+registros[i]["fecha_registro_pi"]+"</th></tr> <tr><th class='success'>FECHA DE VIABILIDAD</th><th  colspan='5'>"+registros[i]["fecha_viabilidad_pi"]+"</th></tr>";  
+                              html+="<thead> <tr> <th class='active'>UNIDAD EJECUTORA</th>  <th class='active' colspan='5'>DETALLE </th> </tr></thead>";
+                              html +="<tr><th class='success'>NOMBRE UNIDAD EJECUTORA</th><th  colspan='5'>"+registros[i]["nombre_ue"]+"</th></tr> <tr><th class='success'>NOMBRE</th><th  colspan='5'>"+registros[i]["nombre_ue"]+"</th></tr>";  
+                              html+="<thead> <tr> <th class='active'>NATURALEZA DE INVERSIÓN</th>  <th class='active' colspan='5'>DETALLE </th> </tr></thead>";
+                              html +="<tr><th class='success'>NATURALEZA DE INVERSIÓN</th><th  colspan='5'>"+registros[i]["nombre_naturaleza_inv"]+"</th></tr> <tr><th class='success'>NOMBRE</th><th  colspan='5'>"+registros[i]["nombre_naturaleza_inv"]+"</th></tr>";  
+
+                              html +="</tbody>";
+                            };    
+                            $("#table-detalleProyectoInversion").html(html);
+                        }
+                    });
+
                 }
                 /*fin listar proyecto de inversion*/
         /*Idioma de datatablet table-sector */
