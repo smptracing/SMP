573484f88bc320138ecb674c6cff9bc4311b9836
Johan <jeus1609@gmail.com>
2017-06-20 10:19:21 -0500

    Reparando grandicimo problema hamely

--- assets/js/Estadisticas/pmi.js
+++ assets/js/Estadisticas/pmi.js
@@ -59,40 +59,38 @@ function EstaProyProvincia() {
     //fin data del proyecto    
 }
 
-  var EstadistMontosPipProv=function()
-  {
-      var MontosPipProv=new Array();
-    html1="";
+var EstadistMontosPipProv = function () {
+    var MontosPipProv = new Array();
+    html1 = "";
     $.ajax({
-     "url":base_url +"index.php/PrincipalPmi/EstadisticaMontoPipProvincias",
-     success:function(respuesta)
-                      {
-                         var registros = eval(respuesta);  
-                      var suma=0;
-                         html1+="<thead> <tr> <th  class='active'><h6>Provincia </h6></th> <th class='active'><h6>Estadistica</h6></th><th colspan='12' class='active'><h6>Montos</h6></th> </tr></thead>"
-                         for (var i = 0; i <registros.length;i++) {
-                            MontosPipProv[i]=registros[i]["MontoProyecto"]; //OPCIONAL, SIRVE PARA IMPRIR LOS MONTOS  EN DATOS PIP PROYECTOS POR PROVINCIA EN LOS DIV
-                               suma=MontosPipProv[i]+suma; //TOTAL MONTO PROYECTOS EN CABECERA
+        "url": base_url + "index.php/PrincipalPmi/EstadisticaMontoPipProvincias",
+        success: function (respuesta) {
+            var registros = eval(respuesta);
+            var suma = 0;
+            html1 += "<thead> <tr> <th  class='active'><h6>Provincia </h6></th> <th class='active'><h6>Estadistica</h6></th><th colspan='12' class='active'><h6>Montos</h6></th> </tr></thead>"
+            for (var i = 0; i < registros.length; i++) {
+                MontosPipProv[i] = registros[i]["MontoProyecto"]; //OPCIONAL, SIRVE PARA IMPRIR LOS MONTOS  EN DATOS PIP PROYECTOS POR PROVINCIA EN LOS DIV
+                suma = MontosPipProv[i] + suma; //TOTAL MONTO PROYECTOS EN CABECERA
 
-                          html1 +="<tbody> <tr><th>"+registros[i]["provincia"]+"</th><th><div class='progress progress_sm'> <div class='progress-bar bg-green progress_sm' role='progressbar' data-transitiongoal='45' style='width: "+registros[i]["Cantidad"]+"%;'></div> </div> </th><th>"+registros[i]["MontoProyecto"]+"</th></tr>";
-                          //alert(suma);
-                           };               
-                             html1 +="</tbody>";
-                         $("#table-estaditMontPIPProv").html(html1);
+                html1 += "<tbody> <tr><th>" + registros[i]["provincia"] + "</th><th><div class='progress progress_sm'> <div class='progress-bar bg-green progress_sm' role='progressbar' data-transitiongoal='45' style='width: " + registros[i]["Cantidad"] + "%;'></div> </div> </th><th>" + registros[i]["MontoProyecto"] + "</th></tr>";
+                //alert(suma);
+            }
+            ;
+            html1 += "</tbody>";
+            $("#table-estaditMontPIPProv").html(html1);
 
-                          document.getElementById("MontoPipAbancay").innerHTML =MontosPipProv[0]; // MONTO TOTAL DE ABANCAY EN EL DIV MontoPipAbancay 
-                          document.getElementById("MontoPipAndahuaylas").innerHTML =MontosPipProv[1];         
-                          document.getElementById("MontoPipAntabamba").innerHTML =MontosPipProv[2];
-                          document.getElementById("MontoPipAymaraes").innerHTML =MontosPipProv[3];
-                          document.getElementById("MontoPipChincheros").innerHTML =MontosPipProv[4];
-                          document.getElementById("MontoPipCotabambas").innerHTML =MontosPipProv[5];
-                          document.getElementById("MontoPipGrau").innerHTML =MontosPipProv[6];
-                          //OBTENER EL MONTO TOTAL DE PROYECTOS EN LA CABECERA
-                          document.getElementById("MontoTotalPip").innerHTML =suma;
-                      }   
-                    });
-  }
-
+            document.getElementById("MontoPipAbancay").innerHTML = MontosPipProv[0]; // MONTO TOTAL DE ABANCAY EN EL DIV MontoPipAbancay
+            document.getElementById("MontoPipAndahuaylas").innerHTML = MontosPipProv[1];
+            document.getElementById("MontoPipAntabamba").innerHTML = MontosPipProv[2];
+            document.getElementById("MontoPipAymaraes").innerHTML = MontosPipProv[3];
+            document.getElementById("MontoPipChincheros").innerHTML = MontosPipProv[4];
+            document.getElementById("MontoPipCotabambas").innerHTML = MontosPipProv[5];
+            document.getElementById("MontoPipGrau").innerHTML = MontosPipProv[6];
+            //OBTENER EL MONTO TOTAL DE PROYECTOS EN LA CABECERA
+            document.getElementById("MontoTotalPip").innerHTML = suma;
+        }
+    });
+};
 
 
 function EstadisticasPorCiclosInversion() {
@@ -133,7 +131,7 @@ function EstadisticasPorCiclosInversion() {
                 //html += "<option value=" + registros[i]["id_subgerencia"] + "> " + registros[i]["denom_subgerencia"] + " </option>";
             }
 
-            porcentaje = Math.round( (total_otros / total_proyectos) * 100);
+            porcentaje = Math.round((total_otros / total_proyectos) * 100);
             sql = "<div class=\"widget_summary\">\n"
                 + "                        <div class=\"w_left w_25\">\n"
                 + "                            <span>Otros</span>\n"
@@ -155,8 +153,9 @@ function EstadisticasPorCiclosInversion() {
             panel_estadistica.append(sql);
         }
     });
+}
 
-  $(document).ready(function () {
+$(document).ready(function () {
     EstaProyProvincia();
     EstadistMontosPipProv();
     EstadisticasPorCiclosInversion();
