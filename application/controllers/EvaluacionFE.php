fee9f6946094c0318258b433c9647830c467ee43
Edison Ponce Torres <101137.ed.iis@gmail.com>
2017-06-23 12:50:31 -0500

    subiendo evaluacion

--- application/controllers/EvaluacionFE.php
+++ application/controllers/EvaluacionFE.php
@@ -1,30 +1,32 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class EvaluacionFE extends CI_Controller {
-	public function __construct(){
-      parent::__construct();
-      $this->load->model('Model_EvaluacionFE');
-	}
-	  public function GetEvaluacionFE(){
-    	if ($this->input->is_ajax_request()) 
-        {
-        $datos=$this->Model_EvaluacionFE->GetEvaluacionFE();
-        echo json_encode($datos);
-        }
-        else
-        {
-          show_404();
+class EvaluacionFE extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Model_EvaluacionFE');
+    }
+
+    public function GetEvaluacionFE()
+    {
+        if ($this->input->is_ajax_request()) {
+            $Etapa = "Evaluación";
+            $datos = $this->Model_EvaluacionFE->GetEvaluacionFE($Etapa);
+            echo json_encode($datos);
+        } else {
+            show_404();
         }
     }
-	public function index()
-	{
-		$this->_load_layout('Front/Formulacion_Evaluacion/frmEvaluacionFE');
-	}
-
-	function _load_layout($template)
+    public function index()
     {
-      $this->load->view('layout/Formulacion_Evaluacion/header');
+        $this->_load_layout('Front/Formulacion_Evaluacion/frmEvaluacionFE');
+    }
+
+    public function _load_layout($template)
+    {
+        $this->load->view('layout/Formulacion_Evaluacion/header');
         $this->load->view($template);
         $this->load->view('layout/Formulacion_Evaluacion/footer');
     }
