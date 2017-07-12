5859795e1c030769bf0dcc1f51259a103a733744
michael101136 <michael101136@gmail.com>
2017-07-12 14:44:47 -0500

    Correcion de sector en la parte de adrministracion
    Correcion de sector en la parte de adrministracion

--- assets/js/administrador/Sector.js
+++ assets/js/administrador/Sector.js
@@ -129,7 +129,7 @@
                           //alert(respuesta);
                          var registros = eval(respuesta);
                             for (var i = 0; i <registros.length;i++) {
-                              var html +="<option value="+registros[i]["id_sector"]+"> "+registros[i]["nombre_sector"]+" </option>";   
+                               html +="<option value="+registros[i]["id_sector"]+"> "+registros[i]["nombre_sector"]+" </option>";   
                             };
                             $("#listaSector").html(html);
                             $("#listaSectorModificar").html(html);//para modificar las entidades
