c3303cf7d8674b636a0d248dd657c1ad42f1bf36
michael101136 <michael101136@gmail.com>
2017-07-13 15:41:15 -0500

    add footer
    add footer

--- application/views/layout/ADMINISTRACION/footer.php
+++ application/views/layout/ADMINISTRACION/footer.php
@@ -11,6 +11,7 @@
     <!-- jQuery -->
     <script src="<?php echo base_url(); ?>assets/vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
+     <script src="<?php echo base_url(); ?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- FastClick -->
     <script src="<?php echo base_url(); ?>assets/vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
