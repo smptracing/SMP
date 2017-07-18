d2d79ce5c97a9979b1d4796a63f4b839c52151c2
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-07-18 09:15:41 -0500

    Refactorización de código en el proceso de registro y edición de tipo de gasto.

--- application/controllers/Tipo_Gasto_FE.php
+++ application/controllers/Tipo_Gasto_FE.php
@@ -1,50 +1,65 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tipo_Gasto_FE extends CI_Controller {
-	public function __construct(){
-      parent::__construct();
-      $this->load->model('Model_TipoGastoFE');
+class Tipo_Gasto_FE extends CI_Controller
+{
+	public function __construct()
+	{
+    	parent::__construct();
+
+    	$this->load->model('Model_TipoGastoFE');
 	}
+
 	public function index()
 	{
 		$ListaTipoGastoFE=$this->Model_TipoGastoFE->ListarTipoGastoFE();
-		//$this->_load_layout('Front/PresupuestoEstudioInversion/TipoGastoFE/index',$data);
+		
 		$this->load->view('layout/Formulacion_Evaluacion/header');
-		$this->load->view('Front/PresupuestoEstudioInversion/TipoGastoFE/index',['ListaTipoGastoFE'=>$ListaTipoGastoFE]);
+		$this->load->view('Front/PresupuestoEstudioInversion/TipoGastoFE/index', ['ListaTipoGastoFE' => $ListaTipoGastoFE]);
 		$this->load->view('layout/Formulacion_Evaluacion/footer');
 	}
+
 	public function insertar()
 	{
 		if($_POST)
 		{
 			$txt_descripcion_tipo =$this->input->post("txt_descripcion_tipo");
+
 	    	$Data=$this->Model_TipoGastoFE->insertar($txt_descripcion_tipo);
+
 	    	$this->session->set_flashdata('correcto', 'Se registró correctamente');
+
 	    	return redirect('/Tipo_Gasto_FE'); 	
 		}
+
 	    return $this->load->view('Front/PresupuestoEstudioInversion/TipoGastoFE/insertar');
 	}
+	
 	public function editar()
 	{
 		if($this->input->post('hdId'))
 		{
-			$id =$this->input->post("hdId");			
-			$txt_descripcion_tipo =$this->input->post("txt_descripcion_tipo");
+			$id=$this->input->post("hdId");
+			$txt_descripcion_tipo=$this->input->post("txt_descripcion_tipo");
+
 	    	$Data=$this->Model_TipoGastoFE->editar($id,$txt_descripcion_tipo);
+
 	    	$this->session->set_flashdata('correcto', 'Se modificó correctamente');
 
 	    	return redirect('/Tipo_Gasto_FE'); 	
 		}
+
 		$id=$this->input->post('id');
 
 		$TipoGastoFE=$this->Model_TipoGastoFE->TipoGastoFE($id)[0];
+	    
 	    return $this->load->view('Front/PresupuestoEstudioInversion/TipoGastoFE/editar',['TipoGastoFE'=>$TipoGastoFE]);		
 	}
+
 	function _load_layout($template)
     {
-      $this->load->view('layout/Formulacion_Evaluacion/header');
-      $this->load->view($template);
-      $this->load->view('layout/Formulacion_Evaluacion/footer');
+		$this->load->view('layout/Formulacion_Evaluacion/header');
+		$this->load->view($template);
+		$this->load->view('layout/Formulacion_Evaluacion/footer');
     }
 }
--- application/models/Model_TipoGastoFE.php
+++ application/models/Model_TipoGastoFE.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Model_TipoGastoFE extends CI_Model
 {
 	public function __construct()
@@ -9,27 +10,27 @@ class Model_TipoGastoFE extends CI_Model
 
 	function ListarTipoGastoFE()
 	{
-	$tipogasto=$this->db->query("execute sp_FETipoGasto_Listar"); 
-	return $tipogasto->result();
+		$tipogasto=$this->db->query("execute sp_FETipoGasto_Listar"); 
+		
+		return $tipogasto->result();
 	} 
+
 	function insertar($txt_descripcion_tipo)
   	{
 	  	$this->db->query("execute sp_FETipoGasto_Insertar'".$txt_descripcion_tipo."'");
   	}
+
   	function TipoGastoFE($id)
 	{
 		$tipogastofe=$this->db->query("select * from FE_TIPO_GASTO where id_tipo_gasto='".$id."'");
 
 		return $tipogastofe->result();
 	}
+
   	function editar($id,$txt_descripcion_tipo)
   	{
 	  	$this->db->query("execute sp_FETipoGasto_Modificar'".$id."','".$txt_descripcion_tipo."'");
+	  	
 	  	return true;
   	}
-  	/*function editar($id,$txt_descripcion_tipo)
-  	{
-	  	$tipogasto=$this->db->query("update FE_TIPO_GASTO  set  desc_tipo_gasto='".$txt_descripcion_tipo."' where id_tipo_gasto='".$id."' ");
-	  	return true;
-  	}*/
 }
