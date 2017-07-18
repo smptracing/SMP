e2f66f141efb9bab9519a614c07135aa0e1ae2d0
hamely <sarmiento101147@gmail.com>
2017-07-18 12:12:22 -0500

    validaciones de los campos de REGISTRAR PRESUPUESTO
    Se validó en el cliente los campos del Formulario Registrar Presupuesto

--- application/views/front/PresupuestoEstudioInversion/FEPresupuesto/insertar.php
+++ application/views/front/PresupuestoEstudioInversion/FEPresupuesto/insertar.php
@@ -1,39 +1,43 @@
 		<br />
 		<form class="form-horizontal"  id="form-addFePresupuesto" action="<?php echo base_url();?>index.php/FE_Presupuesto_Inv/insertar" method="POST">
 				<label>Datos generales</label>
-				<div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
-					<label>Estudio De Inversión</label>
-					<select id="cbx_estudioInversion" name="cbx_estudioInversion" class="form-control" required="">
-						 <option value="">Choose..</option>
-						 <option value="">Choose..</option>
-					</select>
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+						<label>Estudio De Inversión</label>
+						<select id="cbx_estudioInversion" name="cbx_estudioInversion" class="form-control notValidate" required="">
+							 <option value="">Choose..</option>
+							 <option value="">Choose..</option>
+						</select>
+					</div>
 				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-					<label>Sector
-					</label>
-					<input type="text" class="form-control" id="txtSector" name="txtSector" autocomplete="off" placeholder="Sector">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+						<label>Sector
+						</label>
+						<input type="text" class="form-control" id="txtSector" name="txtSector" autocomplete="off" placeholder="Sector" autocomplete="off">
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+						<label>Pliego</label>
+						<input type="text" class="form-control" id="txtPliego" name="txtPliego" placeholder="Pliego" autocomplete="off">
+					</div>
 				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-					<label>Pliego</label>
-					<input type="text" class="form-control" id="txtPliego" name="txtPliego" placeholder="Pliego">
-				</div>
-				</hr>
-				<label></label>
-				<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
-					<label>Descripción Fuente</label>
-					<input type="text" class="form-control" id="txtDescripcionFuente" name="txtDescripcionFuente">
-				</div>
-				<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
-					<label>Correlativo Meta</label>
-					<input type="text" class="form-control" id="txtCorelativoMeta" name="txtCorelativoMeta">
-				</div>
-				<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
-					<label>Año</label>
-					<input type="text" class="form-control" id="txtAnio" name="txtAnio" >
-				</div>
-				<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
-					<br>
-					<button id="btn_agregarPresupuestoInnv" class="btn btn-success">Agregar</button>
+				<div class="row" id="presupuestoFuente">
+					<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
+						<label>Descripción Fuente</label>
+						<input type="text" class="form-control" id="txtDescripcionFuente" name="txtDescripcionFuente" autocomplete="off">
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
+						<label>Correlativo Meta</label>
+						<input type="text" class="form-control" id="txtCorelativoMeta" name="txtCorelativoMeta" autocomplete="off">
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
+						<label>Año</label>
+						<input type="text" class="form-control" id="txtAnio" name="txtAnio" autocomplete="off">
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12 form-group has-feedback">
+						<br>
+						<button id="btn_agregarPresupuestoInnv" class="btn btn-success">Agregar</button>
+					</div>
 				</div>
 				<div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
 					<table id="table-PresupestoFormulacion" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
@@ -50,15 +54,16 @@
 				</div>
 				<div class="form-group">
 					<div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-						<button type="button" class="btn btn-primary">Cancel
+					<button type="submit" class="btn btn-success">Registrar Gasto y proceder con la especificación del mismo
 						</button>
-						<button type="submit" class="btn btn-success">Registrar Gasto y proceder con la especificación del mismo
+						<button  class="btn btn-danger" data-dismiss="modal" onclick="removeModal();">Cancelar
 						</button>
+						
 					</div>
 				</div>
 
 		</form>
-<script type="text/javascript">
+<script>
 	$("#btn_agregarPresupuestoInnv").on('click', function(event)
 	{
 		var htmlTemp='<tr>'+
@@ -70,5 +75,67 @@
 
 		$('#table-PresupestoFormulacion > tbody').append(htmlTemp);
 	});
+
+	$(function()
+	{
+		$('#form-addFePresupuesto').formValidation(
+		{
+			framework: 'bootstrap',
+			excluded: [':disabled', ':hidden', ':not(:visible)', '[class*="notValidate"]'],
+			live: 'enabled',
+			message: '<b style="color: #9d9d9d;">Asegúrese que realmente no necesita este valor.</b>',
+			trigger: null,
+			fields:
+			{
+				txtSector:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Sector" es requerido.</b>'
+						}
+					}
+				},
+				txtPliego:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Pliego" es requerido.</b>'
+						}
+					}
+				},
+				txtCorelativoMeta:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Correlativo Meta" es requerido.</b>'
+						}
+					}
+				},
+				txtAnio:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Año" es requerido.</b>'
+						},
+						regexp:
+	                    {
+	                        regexp: "^([0-9]){4}$",
+	                        message: '<b style="color: red;">El campo "Año" debe ser un número entero.</b>'
+	                    }
+					}
+				}
+			}
+		});	
+	});
+
+	
 
 </script>
