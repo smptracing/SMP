8345ee7e3e720a39c8421ea61e6868f86ed80c54
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-07-31 11:49:01 -0500

    Validación de datos para agregar componente y partida en tiempo de ejecución.

--- application/views/front/Ejecucion/ETComponente/insertar.php
+++ application/views/front/Ejecucion/ETComponente/insertar.php
@@ -1,4 +1,4 @@
-<form class="form-horizontal " id="form-addETComponente" action="<?php echo  base_url();?>index.php/ET_Componente/insertar" method="POST">
+<div class="form-horizontal">
 	<div class="row">
 		<div class="col-md-12 col-sm-12 col-xs-12">
 			<label class="control-label">Expediente técnico</label>
@@ -7,9 +7,9 @@
 			</div>
 		</div>
 	</div>
-	<div class="row" style="margin-top: 4px;">
+	<div id="divAgregarComponente" class="row" style="margin-top: 4px;">
 		<div class="col-md-9 col-sm-9 col-xs-9">
-			<input type="text" class="form-control" id="txtDescripcionComponente" placeholder="Descripción del componente">
+			<input type="text" class="form-control" id="txtDescripcionComponente" name="txtDescripcionComponente" placeholder="Descripción del componente">
 		</div>
 		<div class="col-md-3 col-sm-3 col-xs-3">
 			<input type="button" class="btn btn-info" value="Agregar componente" onclick="agregarComponente();" style="width: 100%;">
@@ -19,25 +19,25 @@
 		<div class="col-md-3 col-sm-3 col-xs-3">
 			<label class="control-label">Descripción partida</label>
 			<div>
-				<input type="text" class="form-control" id="txtDescripcionPartida">
+				<input type="text" class="form-control" id="txtDescripcionPartida" name="txtDescripcionPartida">
 			</div>
 		</div>
 		<div class="col-md-3 col-sm-3 col-xs-3">
 			<label class="control-label">Rendimiento</label>
 			<div>
-				<input type="text" class="form-control" id="txtRendimientoPartida">
+				<input type="text" class="form-control" id="txtRendimientoPartida" name="txtRendimientoPartida">
 			</div>
 		</div>
 		<div class="col-md-3 col-sm-3 col-xs-3">
 			<label class="control-label">Unidad</label>
 			<div>
-				<select id="selectUnidadMedidaPartida" class="form-control"></select>
+				<select id="selectUnidadMedidaPartida" name="selectUnidadMedidaPartida" class="form-control"></select>
 			</div>
 		</div>
 		<div class="col-md-2 col-sm-2 col-xs-2">
-			<label class="control-label">Cant.</label>
+			<label class="control-label">Cantidad</label>
 			<div>
-				<input type="text" class="form-control" id="txtCantidadPartida">
+				<input type="text" class="form-control" id="txtCantidadPartida" name="txtCantidadPartida">
 			</div>
 		</div>
 		<div class="col-md-1 col-sm-1 col-xs-1">
@@ -62,18 +62,28 @@
 			Cerrar ventana
 		</button>
 	</div>
-</form>
-<div id="divDialogoTemporal"></div>
+</div>
 <script>
 	function limpiarArbolCompletoMasOpciones()
 	{
 		$('#divAgregarPartida').hide();
 
 		$('#ulComponenteMetaPartida').find('li').css({ "background-color" : "#ffffff" });
+
+		limpiarText('divAgregarPartida', []);
 	}
 
 	function agregarComponente()
 	{
+		$('#divAgregarComponente').data('formValidation').resetField($('#txtDescripcionComponente'));
+
+		$('#divAgregarComponente').data('formValidation').validate();
+
+		if(!($('#divAgregarComponente').data('formValidation').isValid()))
+		{
+			return;
+		}
+
 		limpiarArbolCompletoMasOpciones();
 
 		var htmlTemp='<li>'+
@@ -149,13 +159,25 @@
 
 	function agregarPartida()
 	{
-		limpiarArbolCompletoMasOpciones();
+		$('#divAgregarPartida').data('formValidation').resetField($('#txtDescripcionPartida'));
+		$('#divAgregarPartida').data('formValidation').resetField($('#txtRendimientoPartida'));
+		$('#divAgregarPartida').data('formValidation').resetField($('#selectUnidadMedidaPartida'));
+		$('#divAgregarPartida').data('formValidation').resetField($('#txtCantidadPartida'));
+
+		$('#divAgregarPartida').data('formValidation').validate();
+
+		if(!($('#divAgregarPartida').data('formValidation').isValid()))
+		{
+			return;
+		}
 
 		var htmlTemp='<li style="color: blue;" class="liPartida">'+
 			$('#txtDescripcionPartida').val()+' | '+$('#txtRendimientoPartida').val()+' | '+$('#selectUnidadMedidaPartida').val()+' | '+$('#txtCantidadPartida').val()+' <input type="button" class="btn btn-default btn-xs" value="-" onclick="eliminarPartida(this);" style="width: 30px;">'+
 		'</li>';
 
 		$($(elementoPadreParaAgregarPartida).find('ul')[0]).append(htmlTemp);
+
+		limpiarArbolCompletoMasOpciones();
 	}
 
 	function eliminarPartida(element)
@@ -165,10 +187,9 @@
 		$(element).parent().remove();
 	}
 
-	/*
 	$(function()
 	{
-		$('#form-addETComponente').formValidation(
+		$('#divAgregarComponente').formValidation(
 		{
 			framework: 'bootstrap',
 			excluded: [':disabled', ':hidden', ':not(:visible)', '[class*="notValidate"]'],
@@ -177,17 +198,74 @@
 			trigger: null,
 			fields:
 			{
-				txtNombreProyectoInversion:
+				txtDescripcionComponente:
 				{
 					validators: 
 					{
 						notEmpty:
 						{
-							message: '<b style="color: red;">El campo "Descripción" es requerido.</b>'
+							message: '<b style="color: red;">El campo "Descripción del componente" es requerido.</b>'
 						}
 					}
 				}
 			}
 		});
-	});*/
+
+		$('#divAgregarPartida').formValidation(
+		{
+			framework: 'bootstrap',
+			excluded: [':disabled', ':hidden', ':not(:visible)', '[class*="notValidate"]'],
+			live: 'enabled',
+			message: '<b style="color: #9d9d9d;">Asegúrese que realmente no necesita este valor.</b>',
+			trigger: null,
+			fields:
+			{
+				txtDescripcionPartida:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Descripción del componente" es requerido.</b>'
+						}
+					}
+				},
+				txtRendimientoPartida:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Descripción del componente" es requerido.</b>'
+						}
+					}
+				},
+				selectUnidadMedidaPartida:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Descripción del componente" es requerido.</b>'
+						}
+					}
+				},
+				txtCantidadPartida:
+				{
+					validators: 
+					{
+						notEmpty:
+						{
+							message: '<b style="color: red;">El campo "Descripción del componente" es requerido.</b>'
+						},
+						regexp:
+	                    {
+	                        regexp: /^(\d+([\.]{1}(\d{1,2})?)?)*$/,
+	                        message: '<b style="color: red;">El campo "Cantidad" debe ser un número.</b>'
+	                    }
+					}
+				}
+			}
+		});
+	});
 </script>
