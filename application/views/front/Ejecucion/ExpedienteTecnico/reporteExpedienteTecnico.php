5531f0dd1874a312d8976d09b19aa92ce77be089
michael101136 <michael101136@gmail.com>
2017-08-06 08:49:16 -0500

    grafico principal  de formulacion y evaluacion
    grafico principal  de formulacion y evaluacion

---
+++ application/views/front/Ejecucion/ExpedienteTecnico/reporteExpedienteTecnico.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/dompdf.css">
+  </head>
+
+<body>
+
+  <header>
+      <table>
+          <tr>
+              <td id="header_texto">
+                  <div>FORMATO FF-01  			GOBIERNO REGIONAL DE APURIMAC</div>
+              </td>
+
+          </tr>
+      </table>
+  </header>
+  <footer>
+      <div id="footer_texto">DIRECTIVA PARA FORMUMACIÓN, EJECUCIÓN Y SUPERVISIÓN DE PROYECTOS EL LA FASE DE INVERSION POR <br/>ADMINISTRACIÓN DIRECTA O ENCARGO </div>
+  </footer>
+
+  <div>
+  	<table id="contenido_border">
+  		<tr>
+              <td id="header_texto">
+                1 Nombre de la Unidad Ejecución
+              </td>
+              <td id="header_texto">
+               fsdjf
+              </td>
+
+        </tr>
+
+  	</table>
+  </div>
+
+
+</body>
+</html>
--- assets/js/Estadisticas/EstadisticasFE.js
+++ assets/js/Estadisticas/EstadisticasFE.js
@@ -1,61 +1,69 @@
-
-
-
+$(document).on("ready" ,function(){
+        
           $.ajax({
                 url:base_url+"/index.php/PrincipalFyE/GetAprobadosEstudio",
+                dataType:"json",
                 type:"POST",
-                success:function(respuesta){
-                  console.log(respuesta);
+                cache:false,
+                success:function(respuesta)
+                {
+	                 var arrayEstudioE=new Array();
+	                 $.each(respuesta,function(index,element)
+	                 {
+	                 	arrayEstudioE[index]=element.Cantidadpip;
+	                 });
+
+	                 var dom = document.getElementById("EstudioEtapa");
+	                 var myChart = echarts.init(dom);
+	                 var app = {};
+	                 option = null;
+	                 option = {
+	                 	title : {
+	                 		text: '',
+	                 		subtext: '',
+	                 		x:'center'
+	                 	},
+	                 	tooltip : {
+	                 		trigger: 'item',
+	                 		formatter: "{a} <br/>{b} : {c} ({d}%)"
+	                 	},
+	                 	legend: {
+	                 		orient: 'horizontal',
+	                 		left: 'left',
+	                 		data: ['Aprobados','Evaluación','Viabilidad','Formulación','Otros']
+	                 	},
+	                 	
+	                 	series : [
+	                 	{
+	                 		name: 'Estudios',
+	                 		type: 'pie',
+	                 		radius : '55%',
+	                 		center: ['50%', '60%'],
+	                 		data:[
+	                 		{value:arrayEstudioE[0], name:'Aprobados'},
+	                 		{value:arrayEstudioE[1], name:'Evaluación'},
+	                 		{value:arrayEstudioE[2], name:'Formulación'},
+	                 		{value:arrayEstudioE[3], name:'Viabilidad'},
+	                 		{value:0, name:'Otros'}
+	                 		],
+	                 		itemStyle: {
+	                 			emphasis: {
+	                 				shadowBlur: 10,
+	                 				shadowOffsetX: 0,
+	                 				shadowColor: 'rgba(0, 0, 0, 0.5)'
+	                 			}
+	                 		}
+	                 	}
+	                 	]
+	                 };
+
+	                 if (option && typeof option === "object") {
+	                 	myChart.setOption(option, true);
+	                 }
                 }
           });
-
-          //estadistica en forma de circulo
-          var dom = document.getElementById("PieFormulacionEvaluacion");
-          var myChart = echarts.init(dom);
-          var app = {};
-          option = null;
-          option = {
-          title : {
-           text: '',
-           subtext: '',
-           x:'center'
-          },
-          tooltip : {
-           trigger: 'item',
-           formatter: "{a} <br/>{b} : {c} ({d}%)"
-          },
-          legend: {
-           orient: 'vertical',
-           left: 'left',
-           data: ['Formulación','Evaluación','Viabilidad','Aprobados','Otros']
-          },
-          series : [
-           {
-               name: '%',
-               type: 'pie',
-               radius : '55%',
-               center: ['50%', '60%'],
-               data:[
-                   {value:335, name:'Formulación'},
-                   {value:310, name:'Evaluación'},
-                   {value:234, name:'Viabilidad'},
-                   {value:135, name:'Aprobados'},
-                   {value:1548, name:'Otros'}
-               ],
-               itemStyle: {
-                   emphasis: {
-                       shadowBlur: 10,
-                       shadowOffsetX: 0,
-                       shadowColor: 'rgba(0, 0, 0, 0.5)'
-                   }
-               }
-           }
-          ]
-          };
-          ;
-          if (option && typeof option === "object") {
-          myChart.setOption(option, true);
-          }
+          
+        
           //Fin estadistica en forma de circulo
 
           var dom = document.getElementById("CenterFEvaluacion");
@@ -137,6 +145,79 @@
               myChart.setOption(option, true);
           }
             //inicio graficos
+             if ("undefined" != typeof Chart && (console.log("init_chart_doughnut"), $(".MontosTiposGasto").length))
+            {
+                var a=
+                {
+                    type: "doughnut",
+                    tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+                    data:
+                    {
+                        labels: ["Abancay", "Andahuaylas", "Antabamba", "Aymaraes", "Contabambas", "Chincheros", "Grau"],
+                        datasets: [
+                        {
+                            data: [1, 2, 3, 4, 5, 6, 7],
+                            backgroundColor: ["#3498DB", "#9B59B6", "#E74C3C", "#26B99A", "#B6CBD6", "#708B99", "#52C5E1"],
+                            hoverBackgroundColor: ["#3498DB", "#B370CF", "#E95E4F", "#36CAAB", "#BDD3DF", "#7C96A3", "#52C5E1"]
+                        }]
+                    },
+                    options: { legend: !1, responsive: !1 }
+                };
+
+                $(".MontosTiposGasto").each(function()
+                {
+                    var b=$(this);
+
+                    new Chart(b, a)
+                });
+            }
 
 
-     
+	                 var dom = document.getElementById("EstudioProvinvia");
+	                 var myChart = echarts.init(dom);
+	                 var app = {};
+	                 option = null;
+	                 option = {
+	                 	title : {
+	                 		text: '',
+	                 		subtext: '',
+	                 		x:'center'
+	                 	},
+	                 	tooltip : {
+	                 		trigger: 'item',
+	                 		formatter: "{a} <br/>{b} : {c} ({d}%)"
+	                 	},
+	                 	legend: {
+	                 		orient: 'horizontal',
+	                 		left: 'left',
+	                 		data: ['Aprobados','Evaluación','Viabilidad','Formulación','Otros']
+	                 	},
+	                 	series : [
+	                 	{
+	                 		name: 'Estudios',
+	                 		type: 'pie',
+	                 		radius : '55%',
+	                 		center: ['50%', '60%'],
+	                 		data:[
+	                 		{value:1, name:'Aprobados'},
+	                 		{value:3, name:'Evaluación'},
+	                 		{value:5, name:'Formulación'},
+	                 		{value:4, name:'Viabilidad'},
+	                 		{value:0, name:'Otros'}
+	                 		],
+	                 		itemStyle: {
+	                 			emphasis: {
+	                 				shadowBlur: 10,
+	                 				shadowOffsetX: 0,
+	                 				shadowColor: 'rgba(0, 0, 0, 0.5)'
+	                 			}
+	                 		}
+	                 	}
+	                 	]
+	                 };
+
+	                 if (option && typeof option === "object") {
+	                 	myChart.setOption(option, true);
+	                 }
+
+     	});
