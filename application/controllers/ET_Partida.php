035509c35caec6c762f1a62d876aee14b115e757
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-08-08 11:17:49 -0500

    Registro automático de ET_DETALLE_PARTIDA al momento de registrar partida.

--- application/controllers/ET_Partida.php
+++ application/controllers/ET_Partida.php
@@ -9,6 +9,8 @@ class ET_Partida extends CI_Controller
 		
 		$this->load->model('Model_ET_Partida');
 		$this->load->model('Model_Unidad_Medida');
+		$this->load->model('Model_ET_Etapa_Ejecucion');
+		$this->load->model('Model_ET_Detalle_Partida');
 	}
 
 	public function insertar()
@@ -35,6 +37,17 @@ class ET_Partida extends CI_Controller
 			$unidadMedida=$this->Model_Unidad_Medida->UnidadMedida($idUnidad)[0];
 
 			$ultimoIdPartida=$this->Model_ET_Partida->ultimoId();
+
+			$etEtapaEjecucion=$this->Model_ET_Etapa_Ejecucion->ETEtapaEjecucionPorDescEtaoaET('Elaboración de expediente técnico');
+
+			if($etEtapaEjecucion==null)
+			{
+				$this->db->trans_rollback();
+
+				echo json_encode(['proceso' => 'Error', 'mensaje' => 'No se encuentra registrado la etapa de "Elaboración de expediente técnico" para proseguir con el proceso. Por favor registre este dato en las etapas de ejecución.']);exit;
+			}
+
+			$this->Model_ET_Detalle_Partida->insertar($ultimoIdPartida, $idUnidad, $etEtapaEjecucion->id_etapa_et, $rendimientoPartida, $cantidadPartida, 0, 0, true);
 
 			$this->db->trans_complete();
 
