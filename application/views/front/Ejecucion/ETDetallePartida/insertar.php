af30e01747816f25c05b156c1c51767010d8008f
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-08-14 12:38:10 -0500

    Muestra de partidas en administración de partidas y analítico.

--- application/views/front/Ejecucion/ETDetallePartida/insertar.php
+++ application/views/front/Ejecucion/ETDetallePartida/insertar.php
@@ -1,3 +1,107 @@
+<?php
+function mostrarMetaAnidada($meta, $idExpedienteTecnico, $listaETEtapaEjecucion, $listaUnidadMedida)
+{
+	$htmlTemp='';
+
+	if(count($meta->childMeta)==0)
+	{
+		foreach($meta->childPartida as $key => $value)
+		{
+			$htmlTemp.='<div class="row">'.
+				'<div class="col-md-12 col-sm-12 col-xs-12">'.
+					'<div class="x_panel">'.
+						'<div class="x_title">'.
+							'<div class="row">'.
+								'<div class="col-md-7 col-sm-7 col-xs-12">'.$value->desc_partida.'</div>'.
+								'<div class="col-md-3 col-sm-3 col-xs-12">'.$value->rendimiento.'</div>'.
+								'<div class="col-md-1 col-sm-1 col-xs-12">'.$value->descripcion.'</div>'.
+								'<div class="col-md-1 col-sm-1 col-xs-12">'.$value->cantidad.'</div>'.
+							'</div>'.
+						'</div>'.
+						'<div class="x_content">'.
+							'<div class="row">'.
+								'<div class="col-md-8 col-sm-8 col-xs-12">'.
+									'<label>Rendimiento</label>'.
+									'<input id="txtRendimiento" name="txtRendimiento" class="form-control col-md-4 col-xs-12"  placeholder="Rendimiento" required="required" autocomplete="off" >'.
+								'</div>'.
+								'<div class="col-md-2 col-sm-2 col-xs-12">'.
+									'<label>Unidad</label>'.
+									'<select id="cobUnidad" name="cobUnidad" class="form-control">';
+
+										foreach($listaUnidadMedida as $value)
+										{
+											$htmlTemp.='<option value="'.$value->id_unidad.'">'.$value->descripcion.'</option>';
+										}
+
+									$htmlTemp.='</select>'.
+								'</div>'.
+								'<div class="col-md-2 col-sm-2 col-xs-12">'.
+									'<label>Cant.</label>'.
+									'<input id="txtCantidad" name="txtCantidad" class="form-control col-md-4 col-xs-12"  placeholder="" required="required" autocomplete="off" >'.
+								'</div>'.
+							'</div>'.
+							'<div class="row">'.
+								'<div class="col-md-2 col-sm-3 col-xs-12">'.
+									'<label>Precio</label>'.
+									'<input id="txtPrecio" name="txtPrecio" class="form-control col-md-4 col-xs-12"  placeholder="" required="required" autocomplete="off" >'.
+								'</div>'.
+								'<div class="col-md-2 col-sm-3 col-xs-12">'.
+									'<label>Total</label>'.
+									'<input id="txtTotal" name="txtTotal" class="form-control col-md-4 col-xs-12"  placeholder="" required="required" autocomplete="off" >'.
+								'</div>'.
+								'<div class="col-md-6 col-sm-3 col-xs-12">'.
+									'<label>Etapa</label>'.
+									'<select id="cbxEtapa1" class="form-control">';
+
+										foreach($listaETEtapaEjecucion as $value)
+										{
+											$htmlTemp.='<option value="'.$value->id_etapa_et.'">'.$value->desc_etapa_et.'</option>';
+										}
+
+									$htmlTemp.='</select>'.
+								'</div>'.
+								'<div class="col-md-2 col-sm-3 col-xs-12">'.
+									'<label>.</label>'.
+									'<input type="button" class="btn btn-info" value="Proceder" style="width: 100%;">'.
+								'</div>'.
+							'</div><br>'.
+							'<div class="row">'.
+								'<table id="table-Partidad" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">'.
+									'<thead>'.
+										'<tr>'.
+											'<td>Rendimiento</td>'.
+											'<td>Und.</td>'.
+											'<td>Cant.</td>'.
+											'<td>Precio U.</td>'.
+											'<td>Fecah R.</td>'.
+											'<td>Etapa.</td>'.
+											'<td>Anl.</td>'.
+											'<td>Total</td>'.
+											'<td></td>'.
+										'</tr>'.
+									'</thead>'.
+									'<tbody>';
+
+									
+
+									$htmlTemp.='</tbody>'.
+								'</table>'.
+							'</div>'.
+						'</div>'.
+					'</div>'.
+				'</div>'.
+			'</div>';
+		}
+	}
+
+	foreach($meta->childMeta as $key => $value)
+	{
+		$htmlTemp.=mostrarMetaAnidada($value, $idExpedienteTecnico, $listaETEtapaEjecucion, $listaUnidadMedida);
+	}
+
+	return $htmlTemp;
+}
+?>
 <form class="form-horizontal">
 	<div class="row">
 		<div class="col-md-12 col-sm-12 col-xs-12">
@@ -18,80 +122,11 @@
 		</div>
 	</div>
 	<hr style="margin-bottom: 4px;margin-top: 4px;">
-	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12">
-			<div class="x_panel">
-				<div class="x_title">
-					<div class="row">
-						<div class="col-md-3 col-sm-3 col-xs-12">Partida Descripción</div>
-						<div class="col-md-3 col-sm-3 col-xs-12">Rendimiento</div>
-						<div class="col-md-3 col-sm-3 col-xs-12">Und.</div>
-						<div class="col-md-3 col-sm-3 col-xs-12">100</div>
-					</div>
-				</div>
-				<div class="x_content">
-					<div class="row">
-						<div class="col-md-8 col-sm-8 col-xs-12">
-							<label>Rendimiento</label>
-							<input id="txtRendimiento" name="txtRendimiento" class="form-control col-md-4 col-xs-12"  placeholder="Rendimiento" required="required" autocomplete="off" >
-						</div>
-						<div class="col-md-2 col-sm-2 col-xs-12">
-							<label>Unidad</label>
-							<select id="cobUnidad" name="cobUnidad" class="form-control"> 
-								<?php foreach($listaUnidadMedida as $value) { ?>
-									<option value="<?=$value->id_unidad?>"><?=$value->descripcion?></option>
-								<?php } ?>
-							</select>
-						</div>
-						<div class="col-md-2 col-sm-2 col-xs-12">
-							<label>Cant.</label>
-							<input id="txtCantidad" name="txtCantidad" class="form-control col-md-4 col-xs-12"  placeholder="" required="required" autocomplete="off" >
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-2 col-sm-3 col-xs-12">
-							<label>Precio</label>
-							<input id="txtPrecio" name="txtPrecio" class="form-control col-md-4 col-xs-12"  placeholder="" required="required" autocomplete="off" >
-						</div>
-						<div class="col-md-2 col-sm-3 col-xs-12">
-							<label>Total</label>
-							<input id="txtTotal" name="txtTotal" class="form-control col-md-4 col-xs-12"  placeholder="" required="required" autocomplete="off" >
-						</div>
-						<div class="col-md-6 col-sm-3 col-xs-12">
-							<label>Etapa</label>
-							<select id="cbxEtapa1" class="form-control"> 
-								<?php foreach($listaETEtapaEjecucion as $value) { ?>
-									<option value="<?=$value->id_etapa_et?>"><?=$value->desc_etapa_et?></option>
-								<?php } ?>
-							</select>
-						</div>
-						<div class="col-md-2 col-sm-3 col-xs-12">
-							<label>.</label>
-							<input type="button" class="btn btn-info" value="Proceder" style="width: 100%;">
-						</div>
-					</div><br>
-					<div class="row">
-						<table id="table-Partidad" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
-							<thead>
-								<tr>
-									<td>Rendimiento</td>
-									<td>Und.</td>
-									<td>Cant.</td>
-									<td>Precio U.</td>
-									<td>Fecah R.</td>
-									<td>Etapa.</td>
-									<td>Anl.</td>
-									<td>Total</td>
-									<td></td>
-								</tr>
-							</thead>
-							<tbody></tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php foreach($etExpedienteTecnico->childComponente as $key => $value){ ?>
+		<?php foreach($value->childMeta as $index => $item){ ?>
+			<?=mostrarMetaAnidada($item, $etExpedienteTecnico->id_et, $listaETEtapaEjecucion, $listaUnidadMedida);?>
+		<?php } ?>
+	<?php } ?>
 	<hr>
 	<div class="row" style="text-align: right;">
 		<button class="btn btn-danger" data-dismiss="modal">
