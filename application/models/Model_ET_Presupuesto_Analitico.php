1f47aed03bdc56ec8f5473e7539084f8de67be22
michael101136 <michael101136@gmail.com>
2017-08-16 12:57:56 -0500

    registro y listado de presupuesto analitico en expediente tecnico
    registro y listado de presupuesto analitico en expediente tecnico

--- application/models/Model_ET_Presupuesto_Analitico.php
+++ application/models/Model_ET_Presupuesto_Analitico.php
@@ -7,13 +7,23 @@ class Model_ET_Presupuesto_Analitico extends CI_Model
 	{
 		parent::__construct();
 	}
-
+	function insertar($opcion,$idClasificador,$hd_id_et,$idPresupuestoEjecucion)
+	{
+		$this->db->query("execute sp_gestionar_ET_Presupuesto_Analitico @opcion='".$opcion."',@id_clasificador='".$idClasificador."', @id_et='".$hd_id_et."',@id_presupuesto_ej='".$idPresupuestoEjecucion."'");
+		return true;
+	}
+	function listar($opcion,$idExpedienteTecnico)
+	{
+		$data=$this->db->query("execute sp_gestionar_ET_Presupuesto_Analitico @opcion='".$opcion."',@id_et='".$idExpedienteTecnico."'");
+		return $data->result();
+	}
 	function ETPresupuestoAnaliticoPorIdET($idET)
 	{
 		$data=$this->db->query("select * from ET_PRESUPUESTO_ANALITICO as ETPA inner join ET_CLASIFICADOR as ETC on ETPA.id_clasificador=ETC.id_clasificador inner join ET_PRESUPUESTO_EJECUCION as ETPE on ETPA.id_presupuesto_ej=ETPE.id_presupuesto_ej where ETPA.id_eT=".$idET);
 
 		return $data->result();
 	}
+
 	function listarPresupuestoAnalitico($id_et)
 	{
 		$ETClasificador=$this->db->query("WITH Clasificador(id_clasificador, id_clasificador_padre, num_clasificador,desc_clasificador, Nivel,precio_parcial) AS
