c84505ec24a4df8c73cf98d7b60b087de670c4cf
Edison Ponce Torres <101137.ed.iis@gmail.com>
2017-08-23 17:24:42 -0500

    Quitando campo estado de  operacion y mantenimiento

--- assets/js/PMI/MetaPip.js
+++ assets/js/PMI/MetaPip.js
@@ -302,7 +302,7 @@ $("#Cbx_AnioCartera").change(function() {
                                     {"data":"nombre_pi"},
                                     {"data":"costo_pi"},
                                     {"data":"nombre_estado_ciclo"},
-                                    {"data": function (data, type, dataToSet) {
+                                 /*   {"data": function (data, type, dataToSet) {
 
                                       if (data.estado_programado !='0') //estap programado
                                       {
@@ -314,7 +314,7 @@ $("#Cbx_AnioCartera").change(function() {
                                         //return '<a  href="#"><button type="button" class="btn btn btn-danger btn-xs">No Programado</button></a>';
                                         return '<h5><span class="label label-danger">No Programado</span></h5>';
                                       }
-                                   }},
+                                   }},*/
                                        {"defaultContent":"<center><button type='button' title='Programar' class='meta_pip btn btn-success btn-xs' data-toggle='modal' data-target='#Ventana_Meta_Presupuestal_PI'><i class='fa fa-usd' aria-hidden='true'></i></button></center>"}
                                 ],
                                "language":idioma_espanol
