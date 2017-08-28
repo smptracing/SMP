f4bad13304872b44cf3cbb384d8cad91b6547cd6
flower919 <florescobar919@gmail.com>
2017-08-28 17:30:00 -0500

    Aagregar menu en el index

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -3,32 +3,27 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Top Navigation</title>
-  <!-- Tell the browser to be responsive to screen width -->
+  <title>SMP TRACING </title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
   <link href="<?php echo base_url(); ?>assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Font Awesome -->
- <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">-->
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link href="<?php echo base_url(); ?>assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/adminlte/ionicons.min.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/adminlte/AdminLTE.min.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/adminlte/_all-skins.min.css" rel="stylesheet">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <!--<link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">-->
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <style>
+    .main-footer {
+    background: #3c8dbc;
+    padding: 15px;
+    color: #fff;
+    border-top: 1px solid #d2d6de;
+  }
+  </style>
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
-<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
 <body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
 
@@ -36,19 +31,29 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="../../index2.html" class="navbar-brand"><b>Admin</b>LTE</a>
+          <a href="../../index2.html" class="navbar-brand"><b>SMP</b> Tracing</a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
         </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
+            <li class="active"><a href="#">Normatividad<span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Guía de Usuarios</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Descargas<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Enlaces<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="#">Action</a></li>
                 <li><a href="#">Another action</a></li>
@@ -60,157 +65,11 @@
               </ul>
             </li>
           </ul>
-          <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
-            </div>
-          </form>
         </div>
         <!-- /.navbar-collapse -->
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-            <!-- Messages: style can be found in dropdown.less-->
-            <li class="dropdown messages-menu">
-              <!-- Menu toggle button -->
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-envelope-o"></i>
-                <span class="label label-success">4</span>
-              </a>
-              <ul class="dropdown-menu">
-                <li class="header">You have 4 messages</li>
-                <li>
-                  <!-- inner menu: contains the messages -->
-                  <ul class="menu">
-                    <li><!-- start message -->
-                      <a href="#">
-                        <div class="pull-left">
-                          <!-- User Image -->
-                          <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                        </div>
-                        <!-- Message title and timestamp -->
-                        <h4>
-                          Support Team
-                          <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                        </h4>
-                        <!-- The message -->
-                        <p>Why not buy a new awesome theme?</p>
-                      </a>
-                    </li>
-                    <!-- end message -->
-                  </ul>
-                  <!-- /.menu -->
-                </li>
-                <li class="footer"><a href="#">See All Messages</a></li>
-              </ul>
-            </li>
-            <!-- /.messages-menu -->
 
-            <!-- Notifications Menu -->
-            <li class="dropdown notifications-menu">
-              <!-- Menu toggle button -->
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-bell-o"></i>
-                <span class="label label-warning">10</span>
-              </a>
-              <ul class="dropdown-menu">
-                <li class="header">You have 10 notifications</li>
-                <li>
-                  <!-- Inner Menu: contains the notifications -->
-                  <ul class="menu">
-                    <li><!-- start notification -->
-                      <a href="#">
-                        <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                      </a>
-                    </li>
-                    <!-- end notification -->
-                  </ul>
-                </li>
-                <li class="footer"><a href="#">View all</a></li>
-              </ul>
-            </li>
-            <!-- Tasks Menu -->
-            <li class="dropdown tasks-menu">
-              <!-- Menu Toggle Button -->
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <i class="fa fa-flag-o"></i>
-                <span class="label label-danger">9</span>
-              </a>
-              <ul class="dropdown-menu">
-                <li class="header">You have 9 tasks</li>
-                <li>
-                  <!-- Inner menu: contains the tasks -->
-                  <ul class="menu">
-                    <li><!-- Task item -->
-                      <a href="#">
-                        <!-- Task title and progress text -->
-                        <h3>
-                          Design some buttons
-                          <small class="pull-right">20%</small>
-                        </h3>
-                        <!-- The progress bar -->
-                        <div class="progress xs">
-                          <!-- Change the css width attribute to simulate progress -->
-                          <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                            <span class="sr-only">20% Complete</span>
-                          </div>
-                        </div>
-                      </a>
-                    </li>
-                    <!-- end task item -->
-                  </ul>
-                </li>
-                <li class="footer">
-                  <a href="#">View all tasks</a>
-                </li>
-              </ul>
-            </li>
-            <!-- User Account Menu -->
-            <li class="dropdown user user-menu">
-              <!-- Menu Toggle Button -->
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <!-- The user image in the navbar-->
-                <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                <span class="hidden-xs">Alexander Pierce</span>
-              </a>
-              <ul class="dropdown-menu">
-                <!-- The user image in the menu -->
-                <li class="user-header">
-                  <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                  <p>
-                    Alexander Pierce - Web Developer
-                    <small>Member since Nov. 2012</small>
-                  </p>
-                </li>
-                <!-- Menu Body -->
-                <li class="user-body">
-                  <div class="row">
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Followers</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Sales</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Friends</a>
-                    </div>
-                  </div>
-                  <!-- /.row -->
-                </li>
-                <!-- Menu Footer-->
-                <li class="user-footer">
-                  <div class="pull-left">
-                    <a href="#" class="btn btn-default btn-flat">Profile</a>
-                  </div>
-                  <div class="pull-right">
-                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
         </div>
         <!-- /.navbar-custom-menu -->
       </div>
@@ -227,7 +86,7 @@
         <ol class="breadcrumb">
         </ol>
       </section>
-       <section class="content">
+       <section class="content" style="margin-bottom: 120px;">
           <div class="box box-default color-palette-box">
             <div class="box-header with-border">
               <h3 class="box-title"><i class="fa fa-refresh"></i> CICLO DE INVERSIONES</h3>
@@ -245,7 +104,6 @@
                                 <i class="fa fa-calendar"></i>
                             </div>
                             <a href="<?php echo site_url('PrincipalPmi/pmi'); ?>" class="small-box-footer">
-                                <!--                                       <a href="#" class="small-box-footer">-->
                                 Ingresar <i class="fa fa-arrow-circle-right"></i>
                             </a>
                         </div>
@@ -262,7 +120,6 @@
                               <i class="fa fa-edit"></i>
                           </div>
                           <a href="<?php echo site_url('PrincipalFyE/PrincipalFyED'); ?>" class="small-box-footer">
-                              <!--                                       <a href="#" class="small-box-footer">-->
                               Ingresar <i class="fa fa-arrow-circle-right"></i>
                           </a>
                       </div>
@@ -277,8 +134,7 @@
                           <div class="icon">
                               <i class="fa fa-play"></i>
                           </div>
-                          <a href="<?php echo site_url('PrincipalEjecucion/PrincipalEjec'); ?>"class="small-box-footer">
-                              <!--                                       <a href="#" class="small-box-footer">-->
+                          <a href="<?php echo site_url('PrincipalEjecucion/PrincipalEjec'); ?>" class="small-box-footer">
                               Ingresar <i class="fa fa-arrow-circle-right"></i>
                           </a>
                       </div>
@@ -289,7 +145,7 @@
             </div>
           <!-- /.row -->
           <div class="row">
-            <div class="col-lg-4 col-xs-6">
+            <div class="col-lg-4 col-xs-12 col-sm-6">
                                 <!-- small box -->
                                 <div class="small-box bg-yellow"><!-- bg-aqua-active #001f3f-->
                                     <div class="inner">
@@ -307,7 +163,7 @@
                                     </a>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-xs-6">
+                            <div class="col-lg-4 col-xs-12 col-sm-6">
                                 <!-- small box -->
                                 <div class="small-box bg-purple">
                                     <div class="inner">
@@ -324,7 +180,7 @@
                                     </a>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-xs-6">
+                            <div class="col-lg-4 col-xs-12 col-sm-6">
                                 <!-- small box -->
                                 <div class="small-box bg-navy">
                                     <div class="inner">
@@ -350,9 +206,8 @@
               <h3 class="box-title"><i class="fa fa-cog"></i> CONFIGURACIÓN DE PARÁMETROS<br> </h3><br>
             </div>
             <div class="row">
-          <div class="col-lg-4 col-xs-6">
-                                <!-- small box -->
-                                <div class="small-box bg-red"><!-- bg-aqua-active #001f3f-->
+          <div class="col-lg-4 col-xs-12 col-sm-6">
+                                <div class="small-box bg-red">
                                     <div class="inner">
                                         <h3>P</h3>
 
@@ -363,14 +218,12 @@
                                     </div>
                                     <a href="<?php echo site_url('PrincipalParametros/parametros'); ?>"
                                        class="small-box-footer">
-                                        <!--                                       <a href="#" class="small-box-footer">-->
                                         Ingresar <i class="fa fa-arrow-circle-right"></i>
                                     </a>
                                 </div>
                             </div>
 
-                            <div class="col-lg-4 col-xs-6">
-                                <!-- small box -->
+                            <div class="col-lg-4 col-xs-12 col-sm-6">
                                 <div class="small-box bg-light-blue">
                                     <div class="inner">
                                         <h3>U</h3>
@@ -380,13 +233,11 @@
                                         <i class="fa fa-users"></i>
                                     </div>
                                     <a href="<?php echo site_url('Usuario/'); ?>"
-                                       class="small-box-footer">              <!--                                       <a href="#" class="small-box-footer">-->
+                                       class="small-box-footer">
                                         Ingresar <i class="fa fa-arrow-circle-right"></i>
                                     </a>
                                 </div>
                             </div>
-                
-            <!-- /.col -->
           </div>
 
           <!-- /.row -->
@@ -397,7 +248,7 @@
     </section>
         </div>
         <!-- /.box -->
-      </section>-->
+      </section>
       <!-- /.content -->
     </div>
     <!-- /.container -->
@@ -408,8 +259,7 @@
       <div class="pull-right hidden-xs">
         <b>Version</b> 2.3.6
       </div>
-      <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-      reserved.
+      <strong>Copyright &copy; 2017-2019 - </strong> Todos los derechos reservados.
     </div>
     <!-- /.container -->
   </footer>
