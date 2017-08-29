a00bfe86d774e223788b43d29b39d69f0748e816
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-08-29 12:47:25 -0500

    Reporte FF11 finalizado.

--- application/views/front/Ejecucion/ExpedienteTecnico/reporteAnalisisPreciosFF11.php
+++ application/views/front/Ejecucion/ExpedienteTecnico/reporteAnalisisPreciosFF11.php
@@ -49,20 +49,20 @@ function obtenerPartidas($meta)
 	</header>
 	<div style="text-align: center;font-size: 11px;margin-top: 15px; border-color: red; margin-left: 10px;"><b>PROY: "<?=$etExpedienteTecnico->nombre_pi;?>"</b></div>
 	<br>
-	<div style="margin-top: -7px;text-align: center;">EXPEDIENTE GLOBAL</div>
+	<div style="font-size: 14px;margin-top: -7px;text-align: center;">EXPEDIENTE GLOBAL</div>
 	<br>
 	<div>
 		<div style="font-size: 9px;">
-			<table>
+			<table style="width: 100%;">
 				<tbody>
 					<?php foreach ($etExpedienteTecnico->childComponente as $key => $value){ ?>
 						<?php foreach($value->childMeta as $index => $item){ ?>
 							<?php $partidas=obtenerPartidas($item); ?>
 							<?php foreach($partidas as $k => $v){ ?>
 								<tr>
-									<td><b><?=$v->numeracion?></b></td>
+									<td style="width: 50px;"><b><?=$v->numeracion?></b></td>
 									<td><b>PARTIDA:</b></td>
-									<td><b><?=$v->desc_partida?></b></td>
+									<td style="width: 550px;"><b><?=$v->desc_partida?></b></td>
 								</tr>
 								<tr>
 									<td></td>
@@ -73,6 +73,63 @@ function obtenerPartidas($meta)
 									<td></td>
 									<td>RENDIMIENTO:</td>
 									<td><?=$v->rendimiento?></td>
+								</tr>
+								<tr>
+									<td></td>
+									<td colspan="2">
+										<table style="width: 100%;">
+											<thead>
+												<tr>
+													<th>DESCRIPCIÓN</th>
+													<th style="text-align: center;width: 100px;">Unidad</th>
+													<th style="text-align: center;width: 70px;">Cantidad</th>
+													<th style="text-align: center;width: 70px;">P. unitario S/.</th>
+													<th style="text-align: center;width: 70px;">Parcial S/.</th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php foreach($v->childDetallePartida as $keyTemp => $valueTemp){ ?>
+													<?php $totalTemp=0; ?>
+													<?php foreach($valueTemp->childAnalisisUnitario as $indexTemp => $itemTemp){ ?>
+														<tr>
+															<td><b><?=strtoupper($itemTemp->desc_recurso)?></b></td>
+															<td colspan="4"></td>
+														</tr>
+														<?php $subTotalTemp=0; ?>
+														<?php foreach($itemTemp->childDetalleAnalisisUnitario as $kTemp => $vTemp){ ?>
+															<?php
+																$subTotalTemp+=$vTemp->precio_parcial;
+																$totalTemp+=$vTemp->precio_parcial;
+															?>
+															<tr>
+																<td><?=$vTemp->desc_detalle_analisis?></td>
+																<td style="text-align: center;"><?=$vTemp->descripcion?></td>
+																<td style="text-align: center;"><?=$vTemp->cantidad?></td>
+																<td style="text-align: center;"><?=$vTemp->precio_unitario?></td>
+																<td style="text-align: center;"><?=$vTemp->precio_parcial?></td>
+															</tr>
+														<?php } ?>
+														<tr>
+															<td colspan="3"></td>
+															<td style="text-align: center;"><b>Sub-total</b></td>
+															<td style="text-align: center;"><b><?=$subTotalTemp?></b></td>
+														</tr>
+													<?php } ?>
+													<tr>
+														<td colspan="3"></td>
+														<td style="text-align: center;">
+															<br>
+															<b>TOTAL</b>
+														</td>
+														<td style="text-align: center;"><b><?=$totalTemp?></b></td>
+													</tr>
+												<?php } ?>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+								<tr>
+									<td colspan="3"><div style="border: 1px solid #f5f5f5;"></div></td>
 								</tr>
 							<?php } ?>
 						<?php }?>
