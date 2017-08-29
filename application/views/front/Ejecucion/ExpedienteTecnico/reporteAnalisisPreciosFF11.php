b130c04bfd39da03db765c79e9587fc94b7e9ec1
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-08-29 09:09:27 -0500

    Cabecera del reporte FF11 realizado.

--- application/views/front/Ejecucion/ExpedienteTecnico/reporteAnalisisPreciosFF11.php
+++ application/views/front/Ejecucion/ExpedienteTecnico/reporteAnalisisPreciosFF11.php
@@ -4,6 +4,29 @@
 	<title>Reporte de AU</title>
 </head>
 <body style="font-family: Helvetica;text-align: center;">
+	<header style="font-size: 12px;margin-top: -44px;width: 100%;">
+		<table style="margin-top: 17px;width: 100%;">
+			<tr>
+				<td style="width: 85px;">
+					<img style="width: 80px;" src="./assets/images/peru.jpg">
+				</td>
+				<td id="header_texto">
+					<div style="text-align: center;"><b>FORMATO FF-11</b></div>
+					<br>
+					<div style="text-align: center;">ANÁLISIS DE PRECIOS UNITARIOS</div>
+				</td>
+				<td style="width: 85px;">
+					<img style="width: 80px;" src="./assets/images/logo.jpg">
+				</td>
+			</tr>
+		</table>
+	</header>
+	<div style="margin-top: -7px;text-align: center;">EXPEDIENTE GLOBAL</div>
+	<div style="text-align: center;font-size: 11px;margin-top: 15px; border-color: red; margin-left: 10px;"><b>PROY: "<?=$etExpedienteTecnico->nombre_pi;?>"</b></div>
+	<div>
+		<div style="font-size: 9px;">
 
+		</div>
+	</div>
 </body>
 </html>
