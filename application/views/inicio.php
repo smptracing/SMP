bb2957fff87e424c94f01940d618d714b01c5ae9
flower919 <florescobar919@gmail.com>
2017-08-29 11:02:41 -0500

    Agregar al inicio pie de pagina y header

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -18,12 +18,44 @@
   <link href="<?php echo base_url(); ?>assets/adminlte/AdminLTE.min.css" rel="stylesheet">
   <link href="<?php echo base_url(); ?>assets/adminlte/_all-skins.min.css" rel="stylesheet">
   <style>
-    .main-footer {
-    background: #3c8dbc;
-    padding: 15px;
-    color: #fff;
-    border-top: 1px solid #d2d6de;
-  }
+    .main-footer 
+    {
+      background: #3c8dbc;
+      padding: 15px;
+      color: #fff;
+      border-top: 1px solid #d2d6de;
+    }
+    .TituloListaFooter
+    {
+      text-shadow: 1px 1px 2px rgba(0,0,0,0.7); 
+      font-size: 14px;
+      color: white;
+      padding-left: 20px;
+      /*padding-bottom: 44px;*/
+    }
+    .tituloHeader
+    {
+      display: inline-block;
+      text-align: middle;
+      padding-top: 25px;
+      color: white;
+      font-size: 15px;
+
+    }
+    .tituloLogo
+    {
+      padding-top: 25px;
+      
+    }
+    .navbar-header
+    {
+      height: 70px;
+    }
+    @media (max-width: 770px) {
+      .tituloHeader{
+        display: none;
+      }
+    }
   </style>
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -37,12 +69,12 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="../../index2.html" class="navbar-brand"><b>SMP</b> Tracing</a>
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+          <a href="../../index2.html" class="navbar-brand tituloLogo"><b style="font-size: 40px; padding-top: 25px;">SMP</b> Tracing v1.0</a>
+          <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
-          </button>
+          </button>-->
         </div>
-        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+        <!--<div class="collapse navbar-collapse pull-left" id="navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Normatividad<span class="caret"></span></a>
@@ -82,7 +114,17 @@
               </ul>
             </li>
           </ul>
+        </div>-->
+
+        <div class="navbar-custom-menu">
+         <span class="tituloHeader">Software de Seguimiento y Monitoreo de PIP's</span><br>
+          <!--<ul class="nav navbar-nav">
+          <span>Software de Seguimiento y Monitoreo de PIP's</span>-->
+          </ul>
         </div>
+
+
+
       </div>
     </nav>
   </header>
@@ -247,10 +289,49 @@
   </div>
   <footer class="main-footer">
     <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.6
+      <div class="row">
+        <div class="col-lg-4 col-sm-6">
+            <span class="TituloListaFooter"><strong>Guía de Usuario</strong></span>
+            <ul>
+                <li><a style="color:#fff;" href="#">Mantenimiento de Parámetros</a></li>
+                <li><a style="color:#fff;" href="#">PMI</a></li>
+                <li><a style="color:#fff;" href="#">FormulaciÓn y EvaluaciÓn</a></li>
+                <li><a style="color:#fff;" href="#">Ejecución</a></li>
+                <li><a style="color:#fff;" href="#">Liquidación</a></li>
+                <li><a style="color:#fff;" href="#">Reportes</a></li>
+                <li><a style="color:#fff;" href="#">Control de Usuarios</a></li>
+            </ul>          
+        </div>
+        <div class="col-lg-4 col-sm-6" >
+            <span class="TituloListaFooter"><strong>Enlaces</strong></span>
+            <ul>
+                <li><a style="color:#fff;" href="http://ofi5.mef.gob.pe/sosem2/">App Sosem</a></li>
+                <li><a style="color:#fff;" href="http://ofi4.mef.gob.pe/odi/login.asp?mensaje=si">Banco de Inversiones</a></li>
+                <li><a style="color:#fff;" href="https://www.mef.gob.pe/es/aplicativos-invierte-pe?id=4279">Consulta de Inversiones</a></li>
+                <li><a style="color:#fff;" href="https://apps4.mineco.gob.pe/sispipapp/">Programación Multianual InviertePE</a></li>
+                <li><a style="color:#fff;" href="http://apps2.mef.gob.pe/consulta-vfp-webapp/consultaExpediente.jspx">Consulta SIAF</a></li>
+            </ul>
+        </div>
+        
+        <div class="col-lg-4 col-sm-6">
+            <span class="TituloListaFooter"><strong>Descargas</strong></span>
+            <ul>
+                <li><a style="color:#fff;" href="https://www.mef.gob.pe/es/anexos-y-formatos#anexos">Anexos  InviertePe</a></li>
+                <li><a style="color:#fff;" href="https://www.mef.gob.pe/es/anexos-y-formatos#formatos">Formatos</a></li>
+            </ul>
+            <span class="TituloListaFooter"><strong>Normatividad</strong></span>
+            <ul>
+                <li><a style="color:#fff;" href="https://www.mef.gob.pe/es/documentacion-sp-30574/temas/sistema-nacional-de-programacion-multianual-y-gestion-de-inversiones-invierte-pe/15837-decreto-supremo-n-027-2017-ef-2/file">Decreto Supremo Nº 027-2017-EF</a></li>
+                <li><a style="color:#fff;" href="https://www.mef.gob.pe/es/documentacion-sp-30574/temas/sistema-nacional-de-programacion-multianual-y-gestion-de-inversiones-invierte-pe/15836-decreto-legislativo-n-1252-1/file">Decreto Legislativo N° 1252</a></li>
+            </ul>          
+        </div>
       </div>
-      <strong>Copyright &copy; 2017-2019 - </strong> Todos los derechos reservados.
+
+
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+      </div>
+      <strong style="padding-left: 20px;">Copyright &copy; 2017-2019 - </strong> Todos los derechos reservados.
     </div>
   </footer>
 </div>
