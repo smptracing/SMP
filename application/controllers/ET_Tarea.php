1a8678c9bbc02fcc1abfb7c6d166b3d1597f6233
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-09-01 15:54:09 -0500

    Registro básico de las actividades de un expediente técnico mediante JQueryGantt.

--- application/controllers/ET_Tarea.php
+++ application/controllers/ET_Tarea.php
@@ -7,22 +7,54 @@ class ET_TAREA extends CI_Controller
 	{
 		parent::__construct();
 
-		//$this->load->model('Model');
+		$this->load->model('Model_ET_Tarea');
+	}
+
+	public function index()
+	{
+		return $this->load->view('Front/Ejecucion/ETTareaGantt/index');
 	}
 
 	public function insertarBloque()
 	{
 		if($this->input->is_ajax_request())
 		{
+			$this->db->trans_start();
+
+			$idTareaGantt=$this->input->post('idTareaGantt');
+
 			$tareas=json_decode($this->input->post('tareas'));
+
+			$this->Model_ET_Tarea->eliminarETTareaPorIdTareaGantt($idTareaGantt);
 
 			foreach($tareas as $key => $value)
 			{
+				$predecesoraTarea='NULL';
+
 				$value->start=date("Y-m-d H:i:s", ($value->start/1000));
 				$value->end=date("Y-m-d H:i:s", ($value->end/1000));
 
-				
+				if($value->level!=0)
+				{
+					$countTemp=0;
+
+					for($i=$key; $i>=0; $i--)
+					{
+						if($tareas[$i]->level!=$value->level && $tareas[$i]->level<$value->level)
+						{
+							$predecesoraTarea=$key-$countTemp+1;
+
+							break;
+						}
+
+						$countTemp++;
+					}
+				}
+
+				$this->Model_ET_Tarea->insertar($idTareaGantt, 'NULL', '', $value->name, $value->start, $value->end, 0, $value->progress, 'green', $value->level, $predecesoraTarea, 0, ($key+1));
 			}
+
+			$this->db->trans_complete();
 
 			echo json_encode(['Correcto' => 'Actividades guardadas correctamente.']);exit;
 		}
