df73a7b9136c0f60f212739c27a575d9e9867adf
jpuentech <jpuentech@gmail.com>
2017-09-07 15:19:14 -0500

    funcion para abrir vista de usuarios

--- application/models/Model_Usuario.php
+++ application/models/Model_Usuario.php
@@ -17,12 +17,19 @@ class Model_Usuario extends CI_Model
               return false;
              }
         }
-        function AddUsuario($id_persona,$txt_usuario,$txt_contrasenia,$cbb_TipoUsuario)
+        function AddUsuario($id_persona,$txt_usuario,$txt_contrasenia,$cbb_TipoUsuario,$cbb_listaMenuDestino)
         {
-		        $this->db->query("execute sp_usuario_c'".$id_persona."','".$txt_usuario."','".$txt_contrasenia."','".$cbb_TipoUsuario."'");
-		        if ($this->db->affected_rows() > 0) {
-		            return true;
-		        } else {
+		        $query=$this->db->query("execute sp_usuario_c'".$id_persona."','".$txt_usuario."','".$txt_contrasenia."','".$cbb_TipoUsuario."'");
+                //if($this->db->affected_rows()>0){
+                if($query){
+                    $arrayMenuUsuario=explode("-",$cbb_listaMenuDestino);
+                    for($i=0;$i<count($arrayMenuUsuario);$i++){
+                        $this->db->close();
+                        $this->db->query("insert into ACCESS_MENU(id_menu,id_persona) values(".$arrayMenuUsuario[$i].",".$id_persona.");");
+                    }
+                    return true;
+		        } 
+                else {
 		            return false;
 		        }
         }
