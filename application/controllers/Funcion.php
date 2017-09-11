71de8da69078b754725369d8302de0001dd216fc
flower919 <florescobar919@gmail.com>
2017-09-11 17:12:40 -0500

    Parsear valores de numero de beneficiarios y costo del poryecto en la tabla

--- application/controllers/Funcion.php
+++ application/controllers/Funcion.php
@@ -80,7 +80,11 @@ class Funcion extends CI_Controller {/* Mantenimiento de sector entidad Y servic
             $fecha2=(($aFecha=='' || $aFecha==null) ? 'NULL' : "'".$aFecha."'");
 
             $datos=$this->Model_Funcion->GetProyectos($idFuncion,$idDivisionFuncional,$idGrupoFuncional,$provincia,$distrito,$fecha1,$fecha2);
-
+            foreach ($datos as $key => $value) 
+            {
+                $value->num_beneficiarios = $this->a_number_format($value->num_beneficiarios , 2, '.',",",3);
+                $value->costo_pi = $this->a_number_format($value->costo_pi , 2, '.',",",3);
+            }
 
             $this->load->view('front/Reporte/CadenaFuncional/tablaFuncion',['listaProyectos'=>$datos]);
         }
@@ -128,10 +132,65 @@ class Funcion extends CI_Controller {/* Mantenimiento de sector entidad Y servic
         }
 
     }
+    function a_number_format($number_in_iso_format, $no_of_decimals=3, $decimals_separator='.', $thousands_separator='', $digits_grouping=3)
+    {
+        // Check input variables
+        if (!is_numeric($number_in_iso_format)){
+            error_log("Warning! Wrong parameter type supplied in my_number_format() function. Parameter \$number_in_iso_format is not a number.");
+            return false;
+        }
+        if (!is_numeric($no_of_decimals)){
+            error_log("Warning! Wrong parameter type supplied in my_number_format() function. Parameter \$no_of_decimals is not a number.");
+            return false;
+        }
+        if (!is_numeric($digits_grouping)){
+            error_log("Warning! Wrong parameter type supplied in my_number_format() function. Parameter \$digits_grouping is not a number.");
+            return false;
+        }
+        
+        
+        // Prepare variables
+        $no_of_decimals = $no_of_decimals * 1;
+        
+        
+        // Explode the string received after DOT sign (this is the ISO separator of decimals)
+        $aux = explode(".", $number_in_iso_format);
+        // Extract decimal and integer parts
+        $integer_part = $aux[0];
+        $decimal_part = isset($aux[1]) ? $aux[1] : '';
+        
+        // Adjust decimal part (increase it, or minimize it)
+        if ($no_of_decimals > 0){
+            // Check actual size of decimal_part
+            // If its length is smaller than number of decimals, add trailing zeros, otherwise round it
+            if (strlen($decimal_part) < $no_of_decimals){
+                $decimal_part = str_pad($decimal_part, $no_of_decimals, "0");
+            } else {
+                $decimal_part = substr($decimal_part, 0, $no_of_decimals);
+            }
+        } else {
+            // Completely eliminate the decimals, if there $no_of_decimals is a negative number
+            $decimals_separator = '';
+            $decimal_part       = '';
+        }
+        
+        // Format the integer part (digits grouping)
+        if ($digits_grouping > 0){
+            $aux = strrev($integer_part);
+            $integer_part = '';
+            for ($i=strlen($aux)-1; $i >= 0 ; $i--){
+                if ( $i % $digits_grouping == 0 && $i != 0){
+                    $integer_part .= "{$aux[$i]}{$thousands_separator}";
+                } else {
+                    $integer_part .= $aux[$i];            
+                }
+            }
+        }
+        
+        $processed_number = "{$integer_part}{$decimals_separator}{$decimal_part}";
+        return $processed_number;
+    }
 
-    /*fin FUNCION*/
-
-    /* division funcional*/
 
     function _load_layout($template)
     {
