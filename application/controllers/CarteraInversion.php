c39354e0ed3ed3822b2dc035d8af7115bcec9ab3
jpuentech <jpuentech@gmail.com>
2017-09-12 11:49:15 -0500

    funcion para editar cartera

--- application/controllers/CarteraInversion.php
+++ application/controllers/CarteraInversion.php
@@ -16,54 +16,76 @@ public function __construct(){
 	//INSERTAR UNA CARTERA DE INVERSION
 	function AddCartera()
 	 {
-	    if ($this->input->is_ajax_request()) 
-	    {
+		if ($this->input->is_ajax_request()) {
+			$config['upload_path']          = './uploads/cartera/';
+		    $config['allowed_types']        = 'pdf|doc|xml|docx|PDF|DOC|DOCX|xls|xlsx';
+		    $config['max_width']            = 1024;
+		    $config['max_height']           = 768;
+		    $config['max_size']      = 15000;
+	        $config['encrypt_name']  = false;
+		    $this->load->library('upload',$config);	
+		    $this->upload->do_upload('Cartera_Resoluacion');
 
-							// echo  $txt_Cartera;  
-		$config['upload_path']          = './uploads/cartera/';
-	    $config['allowed_types']        = 'pdf|doc|xml|docx|PDF|DOC|DOCX|xls|xlsx';
-	    $config['max_width']            = 1024;
-	    $config['max_height']           = 768;
-	    $config['max_size']      = 15000;
-        $config['encrypt_name']  = false;
-
-	    $this->load->library('upload',$config);
-
-		if ( ! $this->upload->do_upload('Cartera_Resoluacion'))
-			   {
-			                	
-			        $error="ERROR NO SE CARGARON  LOS DATOS DE LA CARTERA";
-			        echo $error;
-			   }
-			    else
-			   {
-				                    //$data = array('upload_data' => $this->upload->data());
-
-				                   // $this->load->view('success', $data);
-				                	//$url=-$this->upload->$data['file_name'];
-				  $dateAñoAperturaCart=$this->input->post("dateAñoAperturaCart")."-01-01";
-				  $dateFechaIniCart =$this->input->post("dateFechaIniCart");
-				  $dateFechaFinCart =$this->input->post("dateFechaFinCart");
-				  $estado=0;
-				  $txt_NumResolucionCart =$this->input->post("txt_NumResolucionCart");
-				  $Cartera_Resoluacion=$this->upload->file_name;
-				  $error="corrercto";
-				  if($this->Model_CarteraInversion->AddCartera($dateAñoAperturaCart,$dateFechaIniCart,$dateFechaFinCart,$estado,$txt_NumResolucionCart,$Cartera_Resoluacion)==false)
- 						{
- 							echo "SE REGISTRO LA CARRTERA";
- 						}
- 					//echo $dateAñoAperturaCart,':',':',$dateFechaIniCart,':',$dateFechaFinCart,':',$estado,':',$txt_NumResolucionCart,':',$Cartera_Resoluacion;
-			        
-			      } 						    
-				  }
-				    else
-				    {
-				      show_404();
-				    }
+			
+			  $dateAñoAperturaCart=$this->input->post("dateAñoAperturaCart")."-01-01";
+			  $dateFechaIniCart =$this->input->post("dateFechaIniCart");
+			  $dateFechaFinCart =$this->input->post("dateFechaFinCart");
+			  $estado=0;
+			  $txt_NumResolucionCart =$this->input->post("txt_NumResolucionCart");
+			  $Cartera_Resoluacion=$this->upload->file_name;
+			  if($this->Model_CarteraInversion->AddCartera($dateAñoAperturaCart,$dateFechaIniCart,$dateFechaFinCart,$estado,$txt_NumResolucionCart,$Cartera_Resoluacion)){
+					echo "SE REGISTRO LA CARRTERA...";
+			  }    
+			  else
+		       echo "Error... no se grabaron los datos.";
+		    						    
+	  	}
+	    else{
+	      show_404();
+	    }
 
  	 }
-	//FIN INSERTAR UNA CARTERA DE INVERSION
+ 	 function editCartera($idCartera){
+	    if ($this->input->is_ajax_request()) {
+	    	$config['upload_path']          = './uploads/cartera/';
+		    $config['allowed_types']        = 'pdf|doc|xml|docx|PDF|DOC|DOCX|xls|xlsx';
+		    $config['max_width']            = 1024;
+		    $config['max_height']           = 768;
+		    $config['max_size']      = 15000;
+	        $config['encrypt_name']  = false;
+	        $this->load->library('upload',$config);	
+		    $this->upload->do_upload('Cartera_Resoluacion');
 
+
+	      	$dateAñoAperturaCart=$this->input->post("dateAñoAperturaCart")."-01-01";
+			$dateFechaIniCart =$this->input->post("dateFechaIniCart");
+			$dateFechaFinCart =$this->input->post("dateFechaFinCart");
+			$estado=0;
+			$txt_NumResolucionCart =$this->input->post("txt_NumResolucionCart");
+			$Cartera_Resoluacion=$this->upload->file_name;
+			if($this->Model_CarteraInversion->editCartera($idCartera,$dateAñoAperturaCart,$dateFechaIniCart,$dateFechaFinCart,$estado,$txt_NumResolucionCart,$Cartera_Resoluacion)){
+				echo "SE REGISTRO LA CARRTERA...";
+			}    
+			else
+		       echo "Error... no se grabaron los datos.";  
+		} 
+	     else
+	     {
+	      show_404();
+	     }  
+ 	}
+	//FIN INSERTAR UNA CARTERA DE INVERSION
+  function itemCartera(){
+    	//$data['id_persona']=$this->input->get('id_persona');
+    	if($this->input->get('id_cartera')!=''){
+    		$data['arrayCartera']=$this->Model_CarteraInversion->getCartera($this->input->get('id_cartera'))[0];
+			$this->load->view('Front/Pmi/itemCartera',$data);
+    	}
+    	else{
+			$this->load->view('Front/Pmi/itemCartera');
+    	}
+    	
+	}
 	function getCarteraAnio($anio)//La variable "$anio" se esté recuperando en la vista "Front/Pmi/frmMProyectoInversion"; por tal motivo, no hay que borrar este parámetro.
 	{
 		$this->load->view('layout/Pmi/header');
