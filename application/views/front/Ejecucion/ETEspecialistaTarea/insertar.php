769b82d70035947a45f6d799bcdc25a603289ecf
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-09-13 17:18:33 -0500

    Diseño base para la preparación del evento drag and drop de las especialidades sobre la administración de personal de las actividades del diagrama de gantt de expediente técnico.

--- application/views/front/Ejecucion/ETEspecialistaTarea/insertar.php
+++ application/views/front/Ejecucion/ETEspecialistaTarea/insertar.php
@@ -1,9 +1,57 @@
 <link rel="stylesheet" href="<?=base_url()?>assets/vendors/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/bootstrap-select.css">
+<style>
+	.cajonEspecialidad
+	{
+		background-color: #ffffff;
+		box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.4);
+		cursor: move;
+		display: inline-table;
+		height: 70px;
+		margin: 2px;
+		padding: 4px;
+		text-align: center;
+		user-select: none;
+		vertical-align: middle;
+		width: 170px;
+	}
+
+	.cajonEspecialidad:hover
+	{
+		background: #2f9bfb;
+		color: #ffffff;
+	}
+
+	.cajonEspecialidad > small
+	{
+		display: table-cell;
+		vertical-align: middle;
+	}
+</style>
 <h4 style="padding: 5px;margin: 0px;">Actividad: <span style="color: #2f9bfb;">Sistema de seguimiento y mon</span></h4>
 <hr style="margin: 3px;">
-<?php foreach($listaEspecialidad as $key => $value){ ?>
-	<?=$value->nombre_esp?>
-<?php } ?>
+<div style="padding: 5px;">
+	<table>
+		<tbody>
+			<tr>
+				<td style="background-color: #f5f5f5;text-align: center;width: 200px;">
+					<div style="height: 450px;overflow-y: scroll;">
+						<?php foreach($listaEspecialidad as $key => $value){ ?>
+							<div class="cajonEspecialidad">
+								<small><?=$value->nombre_esp?></small>
+							</div>
+						<?php } ?>
+					</div>
+				</td>
+				<td>
+					
+				</td>
+				<td>
+					
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 <script src="<?=base_url()?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?=base_url()?>assets/dist/js/bootstrap-select.js"></script>
