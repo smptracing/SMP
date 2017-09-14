ac61f36200d086518089636dc6bbd8533c91f98f
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-09-14 09:38:03 -0500

    Corrección en la lista de especialistas requeridos asignados sobre la actividad del diagrama de gantt de expediente técnico.

--- application/models/Model_ET_Especialista_Tarea.php
+++ application/models/Model_ET_Especialista_Tarea.php
@@ -17,7 +17,7 @@ class Model_ET_Especialista_Tarea extends CI_Model
 
 	function ETEspecialistaTareaPorIdTareaET($idTareaET)
 	{
-		$data=$this->db->query("select * from ET_ESPECIALISTA_TAREA as etet inner join ESPECIALIDAD as e on etet.id_esp=e.id_esp left join ET_PER_REQ as etpr on etet.id_per_req=etpr.id_per_req");
+		$data=$this->db->query("select * from ET_ESPECIALISTA_TAREA as etet inner join ESPECIALIDAD as e on etet.id_esp=e.id_esp left join ET_PER_REQ as etpr on etet.id_per_req=etpr.id_per_req where etet.id_tarea_et=$idTareaET");
 
 		return $data->result();
 	}
