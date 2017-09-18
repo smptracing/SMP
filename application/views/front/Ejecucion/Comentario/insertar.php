fe888a7c0a91ad5007b7dbd5a497fc42fc7e25b7
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-09-18 11:32:44 -0500

    Diseño base para el proceso de comentarios en las actividades del diagrama de gantt de expediente técnico.

--- application/views/front/Ejecucion/Comentario/insertar.php
+++ application/views/front/Ejecucion/Comentario/insertar.php
@@ -1,5 +1,50 @@
 <link rel="stylesheet" href="<?=base_url()?>assets/vendors/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/bootstrap-select.css">
-
+<div style="height: 250px;overflow-y: scroll;">
+	<table class="table table-striped">
+		<tbody>
+			<tr>
+				<td>
+					<img src="<?=base_url()?>assets/img/user.png" height="50" width="50" style="background-color: #ffffff;border: 1px solid #cccccc;border-radius: 50px;">
+				</td>
+				<td>
+					<b>Kevin Arnold Arias Figueroa</b><br>
+					<small>Este es un comentario de prueba que se está escribiendo para verificar que el diseño se ajuste correctamente a la visualización del usuario.</small>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="<?=base_url()?>assets/img/user.png" height="50" width="50" style="background-color: #fbfbf5;border: 1px solid #cccccc;border-radius: 50px;">
+				</td>
+				<td>
+					<b>Kevin Arnold Arias Figueroa</b><br>
+					<small>Este es un comentario de prueba que se está escribiendo para verificar que el diseño se ajuste correctamente a la visualización del usuario.</small>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="<?=base_url()?>assets/img/user.png" height="50" width="50" style="background-color: #fbfbf5;border: 1px solid #cccccc;border-radius: 50px;">
+				</td>
+				<td>
+					<b>Kevin Arnold Arias Figueroa</b><br>
+					<small>Este es un comentario de prueba que se está escribiendo para verificar que el diseño se ajuste correctamente a la visualización del usuario.</small>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<hr>
+<div class="row">
+	<div class="col-md-12">
+		<div class="col-md-12">
+			<textarea id="txtDescripcionComentario" rows="3" class="form-control" style="resize: none;"></textarea>
+		</div>
+		<div class="col-md-12" style="margin-top: 4px;">
+			<input type="file" value="Publicar" multiple class="col-md-7">
+			<div class="col-md-1"></div>
+			<input type="button" class="btn btn-primary col-md-4" value="Publicar comentario">
+		</div>
+	</div>
+</div>
 <script src="<?=base_url()?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?=base_url()?>assets/dist/js/bootstrap-select.js"></script>
