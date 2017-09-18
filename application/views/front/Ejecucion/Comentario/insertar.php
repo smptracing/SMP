cd7bb7c1ec2a68417db68ba7f704a0b77c93fd29
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-09-18 11:51:50 -0500

    Agregados en el diseño para realizar comentarios de la actividad del diagrama de gantt de un expediente técnico. Se ha agregado visualizació de fecha de publicación del comentario, visualización de archivos en modo enlace y algunos retoques adicionales de forma general.

--- application/views/front/Ejecucion/Comentario/insertar.php
+++ application/views/front/Ejecucion/Comentario/insertar.php
@@ -4,30 +4,48 @@
 	<table class="table table-striped">
 		<tbody>
 			<tr>
-				<td>
-					<img src="<?=base_url()?>assets/img/user.png" height="50" width="50" style="background-color: #ffffff;border: 1px solid #cccccc;border-radius: 50px;">
+				<td style="padding: 2px;padding-top: 4px;">
+					<img src="<?=base_url()?>assets/img/user.png" height="45" width="45" style="background-color: #ffffff;border: 1px solid #cccccc;border-radius: 50px;">
 				</td>
-				<td>
+				<td style="padding: 4px;padding-left: 0px;">
 					<b>Kevin Arnold Arias Figueroa</b><br>
 					<small>Este es un comentario de prueba que se está escribiendo para verificar que el diseño se ajuste correctamente a la visualización del usuario.</small>
+					<div>
+						<small><b>Archivos adjuntos: </b><a href="#">Archivo de prueba.txt</a>, <a href="#">Archivo 2.png</a></small>
+					</div>
+					<div style="color: #999999;font-size: 9px;text-align: right;">
+						<a href="#" style="color: red;font-size: 10px;">Eliminar</a> | 2017-09-18 07:51:20
+					</div>
 				</td>
 			</tr>
 			<tr>
-				<td>
-					<img src="<?=base_url()?>assets/img/user.png" height="50" width="50" style="background-color: #fbfbf5;border: 1px solid #cccccc;border-radius: 50px;">
+				<td style="padding: 2px;padding-top: 4px;">
+					<img src="<?=base_url()?>assets/img/user.png" height="45" width="45" style="background-color: #fbfbf5;border: 1px solid #cccccc;border-radius: 50px;">
 				</td>
-				<td>
+				<td style="padding: 4px;padding-left: 0px;">
 					<b>Kevin Arnold Arias Figueroa</b><br>
 					<small>Este es un comentario de prueba que se está escribiendo para verificar que el diseño se ajuste correctamente a la visualización del usuario.</small>
+					<div>
+						<small><b>Archivos adjuntos: </b>Ninguno</small>
+					</div>
+					<div style="color: #999999;font-size: 9px;text-align: right;">
+						<a href="#" style="color: red;font-size: 10px;">Eliminar</a> | 2017-09-18 07:51:20
+					</div>
 				</td>
 			</tr>
 			<tr>
-				<td>
-					<img src="<?=base_url()?>assets/img/user.png" height="50" width="50" style="background-color: #fbfbf5;border: 1px solid #cccccc;border-radius: 50px;">
+				<td style="padding: 2px;padding-top: 4px;">
+					<img src="<?=base_url()?>assets/img/user.png" height="45" width="45" style="background-color: #fbfbf5;border: 1px solid #cccccc;border-radius: 50px;">
 				</td>
-				<td>
+				<td style="padding: 4px;padding-left: 0px;">
 					<b>Kevin Arnold Arias Figueroa</b><br>
 					<small>Este es un comentario de prueba que se está escribiendo para verificar que el diseño se ajuste correctamente a la visualización del usuario.</small>
+					<div>
+						<small><b>Archivos adjuntos: </b>Ninguno</small>
+					</div>
+					<div style="color: #999999;font-size: 9px;text-align: right;">
+						<a href="#" style="color: red;font-size: 10px;">Eliminar</a> | 2017-09-18 07:51:20
+					</div>
 				</td>
 			</tr>
 		</tbody>
@@ -37,7 +55,7 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="col-md-12">
-			<textarea id="txtDescripcionComentario" rows="3" class="form-control" style="resize: none;"></textarea>
+			<textarea id="txtDescripcionComentario" rows="3" class="form-control" style="resize: none;" placeholder="Escribe un comentario con respecto a la actividad actualmente seleccionada."></textarea>
 		</div>
 		<div class="col-md-12" style="margin-top: 4px;">
 			<input type="file" value="Publicar" multiple class="col-md-7">
