1264e42086d09d7ef46d8f7c8d5343e977780114
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-09-25 16:39:53 -0500

    Visualización de componentes y metas en la tabla de la vista "http://localhost:8080/smp/index.php/Expediente_Tecnico/valorizacionEjecucionProyecto/1".

--- application/views/front/Ejecucion/ExpedienteTecnico/valorizacionEjecucionProyecto.php
+++ application/views/front/Ejecucion/ExpedienteTecnico/valorizacionEjecucionProyecto.php
@@ -1,3 +1,16 @@
+<style>
+	#tableValorizacion
+	{
+
+	}
+
+	#tableValorizacion th, #tableValorizacion td
+	{
+		border: 1px solid #999999;
+		padding: 4px;
+		vertical-align: middle;
+	}
+</style>
 <div class="right_col" role="main">
 	<div>
 		<div class="clearfix"></div>
@@ -8,10 +21,50 @@
 					<div class="clearfix"></div>
 				</div>
 				<div class="x_content">
-					<?php foreach($expedienteTecnico->childComponente as $key => $value)
-					{
-						echo $value->descripcion;
-					} ?>
+					<div id="divContenedorGeneral" style="overflow-x: scroll;">
+						<table id="tableValorizacion">
+							<thead>
+								<tr>
+									<th>PROY:</th>
+									<th colspan="5"><?=$expedienteTecnico->nombre_pi?></th>
+									<th>CRONOGRAMA VALORIZADO DE EJECUCIÓN DEL PROYECTO</th>
+								</tr>
+								<tr>
+									<th style="text-align: center;">ÍTEM</th>
+									<th>DESCRIPCIÓN</th>
+									<th style="text-align: center;">UND.</th>
+									<th style="text-align: center;">CANT.</th>
+									<th style="text-align: center;">P.U.</th>
+									<th style="text-align: center;">TOTAL</th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($expedienteTecnico->childComponente as $key => $value){ ?>
+									<tr>
+										<td style="text-align: center;"><?=$value->numeracion?></td>
+										<td><?=$value->descripcion?></td>
+										<td style="text-align: center;"></td>
+										<td style="text-align: center;"></td>
+										<td style="text-align: center;"></td>
+										<td style="text-align: center;"></td>
+										<td></td>
+									</tr>
+									<?php foreach($value->childMeta as $index => $item){ ?>
+										<tr>
+											<td style="text-align: center;"><?=$item->numeracion?></td>
+											<td><?=$item->desc_meta?></td>
+											<td style="text-align: center;"></td>
+											<td style="text-align: center;"></td>
+											<td style="text-align: center;"></td>
+											<td style="text-align: center;"></td>
+											<td></td>
+										</tr>
+									<?php } ?>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
