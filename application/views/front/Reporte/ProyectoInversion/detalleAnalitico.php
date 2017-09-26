402547f74de98dc71704374c2c5d65a4eec95c23
hamely <sarmiento101147@gmail.com>
2017-09-26 08:42:54 -0500

    Avance Financiero: Se agrego devengado, compromiso, pim acumulado, girado pagado
    Avance Financiero: Se agrego devengado, compromiso, pim acumulado,
    girado pagado

--- application/views/front/Reporte/ProyectoInversion/detalleAnalitico.php
+++ application/views/front/Reporte/ProyectoInversion/detalleAnalitico.php
@@ -38,22 +38,21 @@
 									<div class="row">  
 										<div class="col-md-12 col-sm-12 col-xs-12">
 											
-											<table id="table-DetalleAnaliticoAvance"   border="2px"  cellspacing="0" width="100%">
+											<table id="table-DetalleAnaliticoAvance" class="table table-striped jambo_table bulk_action  table-hover" cellspacing="0" width="100%">
 												<thead>
 
-													<!--<tr>
-														<td>Año</td>
-														<td>Nombre</td>
-														<td>Proyecto</td>
-														<td>Finalidad</td>
-														<td>Nombre de Finalidad</td>
-														<td>Meta</td>
-														<td>Programa</td>
-														<td>Nombre del Programa</td>
-														<td>Sub Programa</td>
-														<td>Sub Programa Nombre</td>
-														<td>Funcion</td>
-													</tr>-->
+													<tr>
+														<td>Prod/Proy. Cat.Pptal. Act/AI/Obr Fun. Div.Fn. Finalidad Meta A Gen</td>
+														<td>Presupuesto</td>
+														<td>Modificación</td>
+														<td>Pim Acumulado</td>
+														<td>Ejecución</td>
+														<td>Compromiso</td>
+														<td>Certificado</td>
+														<td>Devengado</td>
+														<td>Girado</td>
+														<td>Pagado</td>
+													</tr>
 												</thead>
 												<tbody>
 													
@@ -98,36 +97,98 @@
 												
 													<tr>
 														<td><?=$listaDetalleAnaliticoAvancFinE->act_proy ,' ',$listaDetalleAnaliticoAvancFinE->proyecto;?>  </td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
 													</tr>
 			
 													<tr>
 													
-														<td><?=' -------------',$listaDetalleAnaliticoAvancFinE->meta,' ', $listaDetalleAnaliticoAvancFinE->proyecto;?>  </td>
+														<td><?=$listaDetalleAnaliticoAvancFinE->meta,' ', $listaDetalleAnaliticoAvancFinE->proyecto;?>  </td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
 													</tr>
 												
 													<tr>
 											
-														<td><?=' -------------------',$listaDetalleAnaliticoAvancFinE->programa,' ', $listaDetalleAnaliticoAvancFinE->programa_nombre;?>  </td>
+														<td><?=$listaDetalleAnaliticoAvancFinE->programa,' ', $listaDetalleAnaliticoAvancFinE->programa_nombre;?>  </td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
 													</tr>
 													<tr>
 											
-														<td><?=' -------------------------',$listaDetalleAnaliticoAvancFinE->sub_programa,' ', $listaDetalleAnaliticoAvancFinE->sub_programa_nombre;?>  </td>
+														<td><?=$listaDetalleAnaliticoAvancFinE->sub_programa,' ', $listaDetalleAnaliticoAvancFinE->sub_programa_nombre;?>  </td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
 													</tr>
 													<tr>
 											
-														<td><?=' --------------------------------',$listaDetalleAnaliticoAvancFinE->funcion,' ', $listaDetalleAnaliticoAvancFinE->funcion_nombre;?>  </td>
+														<td><?=$listaDetalleAnaliticoAvancFinE->funcion,' ', $listaDetalleAnaliticoAvancFinE->funcion_nombre;?>  </td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
 													</tr>
 													<tr>
 											
-														<td><?=' ------------------------------------',$listaDetalleAnaliticoAvancFinE->finalidad,' ', $listaDetalleAnaliticoAvancFinE->finalidad_nombre;?>  </td>
+														<td><?=$listaDetalleAnaliticoAvancFinE->finalidad,' ', $listaDetalleAnaliticoAvancFinE->finalidad_nombre;?>  </td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
+														<td></td>
 													</tr>
 													<?php foreach($listaDetalleAnaliticoAvancFin as $item ){ ?>
 													
 													  <tr>
 															<td>
-																<?=' ------------------------------------------------',$item->finalidad,' ',$item->finalidad_nombre?>
+																<?=$item->finalidad,' ',$item->finalidad_nombre?>
 													    	</td>
-										
+													    	<td><?=$item->presupuesto?></td>
+													    	<td><?=$item->modificacion?></td>
+															<td><?=$item->pim_acumulado?></td>
+															<td><?=$item->ejecucion?></td>
+															<td><?=$item->compromiso?></td>
+															<td><?=$item->certificado?></td>
+															<td><?=$item->devengado?></td>
+															<td><?=$item->girado?></td>
+															<td><?=$item->pagado?></td>
 													  </tr>
 													<?php } ?>
 
