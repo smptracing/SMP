19c0bed5b72510cd89269ef08917201b637ae0be
hamely <sarmiento101147@gmail.com>
2017-09-28 17:49:15 -0500

    REPORTE DE CLASIFICADOR POR CADA PIP EN EL MODULO DE REPORTES
    REPORTE DE CLASIFICADOR POR CADA PIP EN EL MODULO DE REPORTES

--- application/views/front/Reporte/ProyectoInversion/detalleClasificador.php
+++ application/views/front/Reporte/ProyectoInversion/detalleClasificador.php
@@ -1,9 +1,9 @@
 <style>
 	.modal-dialog
 	{
-		width: 90%;
+		width: 100%;
 		margin: 0;
-		margin-left: 5%;
+		margin-left: 0%;
 		padding: 0;
 	}
 
@@ -33,31 +33,25 @@
 							<div id="myTabContent" class="tab-content">
 								<!-- /Contenido del sector -->
 								<div role="tabpanel" class="tab-pane fade active in" id="tab_Sector" aria-labelledby="home-tab">
-						
+									<div>
+										
+										<!--<?php foreach ($temp as  $value) {?>
+											<?= $value->cod_tt; ?>
+											<?php foreach ($value->child as $key =>  $a) {?>
+											<?= $a->generica; ?>
+			
+											<?php } ?>
+										<?php } ?>-->
+
+									</div>
 									<div class="row">  
 										<div class="col-md-12 col-sm-12 col-xs-12">
-											 <div id="DetalleClasificador" class="table-responsive">
-											<table id="table-DetalleClasificador"  class="table table-striped jambo_table bulk_action  table-hover" cellspacing="0" width="100%">
+											 <div id="DetalleClasificador" class="table-responsive" style="font-size:9.5px">
+											<table id="table-DetalleClasificador" cellspacing="0" width="100%">
 												<thead>
 													<tr>
-														<!--<td>Tipo de transacción</td>
-														<td>Generica</td>
-														<td>Descripción Generica</td>-->
-														<td>Sub generica</td>
-														<td>Descripción Sub generica</td>
-														<td>Específica </td>
-														<td>Descripción Específica</td>
-														<td>Específica Det</td>
-														<td>Descripción Específica Det</td>
-														<td>Ejecución</td>
-														<td>Anulacion</td>
-														<td>Compromiso</td>
-														<td>Devengado</td>
-														<td>Girado</td>
-														<td>Pagado</td>
-														<td>Comprometido Anual</td>
-														<td>Certificado</td>
-														<td>Ene</td>
+														<td>Datos</td>
+														<td>En</td>
 														<td>Feb</td>
 														<td>Mar</td>
 														<td>Abr</td>
@@ -69,103 +63,135 @@
 														<td>Oct</td>
 														<td>Nov</td>
 														<td>Dic</td>
-
+														<td>D</td>
+														<td>C</td>
+														<td>G</td>
+														<td>P</td>
+														<td>Comprom Anu.</td>
+														<td>Cert</td>
+														<td>Ejec</td>
+														<td>Anul</td>
 													</tr>
 												</thead>
 												<tbody>
+												
+											<?php foreach ($temp as  $value) {?>
+												<tr>
+													<td colspan="21">
+														<?= $value->cod_tt,' ', $value->tipo_transaccion; ?>
+													</td>
+											
+												</tr>
+													<?php foreach ($value->child as $key =>  $value1) {?>
+													<tr>
+														<td colspan="21">
+														<?='&nbsp&nbsp&nbsp', $value1->generica,' ',$value1->desc_generica ; ?>
+														</td>
+													
+													</tr>
+														<?php foreach ($value1->child as $key =>  $value2) {?>
+														<tr>
+															<td colspan="21">
+															<?='&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp', $value2->sub_generica,' ',$value2->desc_sub_generica ; ?>
+															</td>
+														
+														</tr>
 
-													<?php foreach($listaDetalleClasificador as $item ){ ?>
-													  	<tr>
-															<!--<td>
-																<?=$item->tipo_transaccion?>
-													    	</td>
-													    	<td>
-																<?=$item->generica?>
-													    	</td>
-													    	<td>
-																<?=$item->desc_generica?>
-													    	</td>-->
-													    	<td>
-																<?=$item->sub_generica?>
-													    	</td>
-													    	<td>
-																<?=$item->des_sub_generica_det?>
-													    	</td>
-													    	<td>
-																<?=$item->especifica?>
-													    	</td>
-													    	<td>
-																<?=$item->desc_especifica?>
-													    	</td>
-													    	<td>
-																<?=$item->especifica_det?>
-													    	</td>
-													    	<td>
-																<?=$item->desc_especifica_det?>
-													    	</td>
-													    	<td>
-																<?=$item->ejecucion?>
-													    	</td>
-													    	<td>
-																<?=$item->anulacion?>
-													    	</td>
-													    	<td>
-																<?=$item->compromiso?>
-													    	</td>
-													    	<td>
-																<?=$item->devengado?>
-													    	</td>
-													    	<td>
-																<?=$item->girado?>
-													    	</td>
-													    	<td>
-																<?=$item->pagado?>
-													    	</td>
-													    	<td>
-																<?=$item->comprometido_anual?>
-													    	</td>
-													    	<td>
-																<?=$item->certificado?>
-													    	</td>
-													    	<td>
-																<?=$item->ene?>
-													    	</td>
-													    	<td>
-																<?=$item->feb?>
-													    	</td>
-													    	<td>
-																<?=$item->mar?>
-													    	</td>
-													    	<td>
-																<?=$item->abr?>
-													    	</td>
-													    	<td>
-																<?=$item->may?>
-													    	</td>
-													    	<td>
-																<?=$item->jun?>
-													    	</td>
-													    	<td>
-																<?=$item->jul?>
-													    	</td>
-													    	<td>
-																<?=$item->ago?>
-													    	</td>
-													    	<td>
-																<?=$item->sep?>
-													    	</td>
-													    	<td>
-																<?=$item->oct?>
-													    	</td>
-													    	<td>
-																<?=$item->nov?>
-													    	</td>
-													    	<td>
-																<?=$item->dic?>
-													    	</td>
+															<?php foreach ($value2->child as $key =>  $value3) {?>
+															<tr>
+																<td colspan="21">
+																<?='&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp', $value3->sub_generica_det,' ',$value3->des_sub_generica_det ; ?>
+																</td>
+															
+															</tr>
 
-													  </tr>
+																<?php foreach ($value3->child as $key =>  $value4) {?>
+																	<tr>
+																		<td colspan="21">
+																		<?='&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp', $value4->especifica,' ',$value4->desc_especifica ; ?>
+																		</td>
+																	
+																	</tr>
+																	
+																	<?php foreach ($value4->child as $key =>  $value5) {?>
+																		<tr>
+																			<td>
+																			<?='&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp', $value5->especifica_det,' ',$value5->desc_especifica_det ; ?>
+																			</td>
+																			<td>
+																			<?= $value5->ene; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->feb; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->mar; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->abr; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->may; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->jun; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->jul; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->ago; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->sep; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->oct; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->nov; ?> 
+																			</td>
+																			<td>
+																			<?= $value5->dic; ?> 
+																			</td>
+																			<td>
+																				<?=$value5->devengado; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->compromiso; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->girado; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->pagado; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->comprometido_anual; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->certificado; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->ejecucion; ?>
+																	    	</td>
+																	    	<td>
+																				<?=$value5->anulacion; ?>
+																	    	</td>
+																		</tr>
+
+																	<?php } ?>
+																	
+																<?php } ?>
+
+															<?php } ?>
+
+														<?php } ?>
+
 													<?php } ?>
+											<?php } ?>
+											
+		
 												</tbody>
 											
 											</table>
