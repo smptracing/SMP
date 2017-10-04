eb4e5c61516b66b8f3e3abb9fb08b4ecbe4a7b81
michael101136 <michael101136@gmail.com>
2017-10-04 12:25:56 -0500

    correcion  en el la subida del archivo
    correcion  en el la subida del archivo

--- application/controllers/PmiCriterioG.php
+++ application/controllers/PmiCriterioG.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PmiCriterioG extends CI_Controller {/* Mantenimiento de sector entidad Y servicio publico asociado*/
+class PmiCriterioG extends CI_Controller {
 
 	public function __construct(){
       parent::__construct();
