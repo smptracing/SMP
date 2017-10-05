988d21e51273d603893d016f931ba7c408aa80dd
flower919 <florescobar919@gmail.com>
2017-10-05 11:34:59 -0500

    Vista para detalle expediente

--- application/views/front/Ejecucion/ExpedienteTecnico/verdetalle.php
+++ application/views/front/Ejecucion/ExpedienteTecnico/verdetalle.php
@@ -23,32 +23,32 @@
 	                        		<a role="menuitem" tabindex="-1" href="#" onclick="window.open(base_url+'index.php/ET_Tarea/index/<?=$ExpedienteTecnicoElaboracion[0]->id_et?>', '_blank'); return false;"> Gestionar Actividades
 	                        		</a>
 		                        </li>
-                        		<li>
+                        		<li role="presentation">
 	                        		<a role="menuitem" tabindex="-1" href="#" class="editar" onclick="paginaAjaxDialogo(null, 'Registro de componentes, metas y partidas', { idExpedienteTecnico : <?=$ExpedienteTecnicoElaboracion[0]->id_et?> }, base_url+'index.php/ET_Componente/insertar', 'GET', null, null, false, true); return false;" >Componentes, Metas y Partidas	
 		                        	</a>
 		                        </li>
-		                        <li>
+		                        <li role="presentation">
 	                        		<a role="menuitem" tabindex="-1" href="#" onclick="paginaAjaxDialogo(null, 'Presupuesto analítico', { idExpedienteTecnico : <?=$ExpedienteTecnicoElaboracion[0]->id_et?> }, base_url+'index.php/ET_Presupuesto_Analitico/insertar', 'GET', null, null, false, true); return false;"> Presupuesto Analítico	
 		                        	</a>
 		                        </li>
-		                        <li>
+		                        <li role="presentation">
 	                        		<a role="menuitem" tabindex="-1" href="#"  onclick="paginaAjaxDialogo(null, 'Seleccionar etapa de ejecución para la clonación', { idExpedienteTecnico : <?=$ExpedienteTecnicoElaboracion[0]->id_et?> }, base_url+'index.php/Expediente_Tecnico/clonar', 'POST', null, null, false, true); return false;"> 		Enviar E.T. a la siguiente etapa	
 		                        	</a>
 		                        </li>
-		                        <li>
+		                        <li role="presentation">
 	                        		<a role="menuitem" tabindex="-1" href="#" onclick="paginaAjaxDialogo(null, 'Visto Bueno del E.T.', { id_ExpedienteTecnico : <?=$ExpedienteTecnicoElaboracion[0]->id_et?> }, base_url+'index.php/Expediente_Tecnico/vistoBueno','GET', null, null, false, true); return false;"> 	Dar Visto Bueno	
 		                        	</a>
 		                        </li>
-		                        <li>
+		                        <li role="presentation">
 	                        		<a role="menuitem" tabindex="-1" href="#" onclick="window.open(base_url+'index.php/Expediente_Tecnico/valorizacionEjecucionProyecto/<?=$ExpedienteTecnicoElaboracion[0]->id_et?>', '_blank'); return false;"> Valorización de 			Ejecución.	
 		                        	</a>
 		                        </li>
 	                        	<li role="presentation" class="divider"></li>
-	                        	<li>
+	                        	<li role="presentation">
 		                        	<a role="menuitem" tabindex="-1" href="#" onclick="paginaAjaxDialogo(null, 'Modificar Expediente Técnico',{ id_et: '<?=$ExpedienteTecnicoElaboracion[0]->id_et?>' }, base_url+'index.php/Expediente_Tecnico/editar', 'GET', null, null, false, true);return false;">Editar Expediente Técnico 	
 		                        	</a> 
 	                        	</li>
-	                        	<li>
+	                        	<li  role="presentation">
 	                        		<a role="menuitem" tabindex="-1" class='eliminarExpediente' href="#" onclick="Eliminar(<?=$ExpedienteTecnicoElaboracion[0]->id_et?>);return false;"> Eliminar Expediente Técnico 	
 		                        	</a>
 		                        </li>
@@ -150,12 +150,6 @@
                     </form>
 				</div>
 			</div>
-			<style>
-				.btn-app{
-					height: 20px;
-					width: 30px;
-				}
-			</style>
 			<div class="x_panel">
                   <div class="x_title">
                     <h2>Button App Design</h2>
@@ -170,27 +164,27 @@
                       <i class="fa fa-trash"></i> Eliminar
                     </a>
                     <a class="btn btn-app" data-toggle="tooltip" title="Asignar Personal">
-                      <i class="fa fa-pause"></i> Personal
+                      <i class="fa fa-user-plus"></i> Personal
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Gestionar Actividades" >
-                      <i class="fa fa-save"></i> Actividades
+                      <i class="fa fa-calendar-o"></i> Actividades
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Registrar Componentes, Metas y Partidas">
                       <i class="fa fa-edit"></i> Registrar
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Presupuesto  Analitico">
-                      <i class="fa fa-play"></i> Presupuesto
+                      <i class="fa fa-money"></i> Presupuesto
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Enviar E.T. a la siguiente etapa">
-                      <i class="fa fa-pause"></i> Enviar E.T.
+                      <i class="fa fa-send"></i> Enviar E.T.
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Dar visto bueno">
-                      <i class="fa fa-save"></i> visto bueno
+                      <i class="fa fa-check-square"></i> visto bueno
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Valorizacion de Ejecucion">
-                      <i class="fa fa-edit"></i> Valorizacion
+                      <i class="fa fa-list-ol"></i> Valorizacion
                     </a>
-                    <a class="btn btn-app"  data-toggle="tooltip" title="Expediente Tecnico 001">
+                    <!--<a class="btn btn-app"  data-toggle="tooltip" title="Expediente Tecnico 001">
                       <i class="fa fa-play"></i> Expediente Tecnico 001
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Metrado">
@@ -204,70 +198,122 @@
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Formato FF-11">
                       <i class="fa fa-repeat"></i> Formato FF-11
-                    </a>
+                    </a>-->
                     <a class="btn btn-app"  data-toggle="tooltip" title="Responsable">
-                      <i class="fa fa-users"></i> Responsable
+                      <i class="fa fa-user"></i> Responsable
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Documentos">
-                      <i class="fa fa-inbox"></i> Documentos
+                      <i class="fa fa-file"></i> Documentos
                     </a>
                     <a class="btn btn-app"  data-toggle="tooltip" title="Detalle Expediente">
-                      <i class="fa fa-envelope"></i> Detalle Expediente
+                      <i class="fa fa-list"></i> Detalle
                     </a>
-                    <br /><br />
+                    <!--<a class="btn btn-app dropdown-toggle"  data-toggle="dropdown" role="button" aria-expanded="false">
+                      <i class="fa fa-envelope"></i> Detalle
+                      <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settingsdsds 1</a>
+                          </li>
+                          <li><a href="#">Settinsdsdgs 2</a>
+                          </li>
+                        </ul>
+                    </a>-->
+                    <!--<ul class="nav panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-danger">Reportes</button>
+                      <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                      </button>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Action</a>
+                        </li>
+                        <li><a href="#">Another action</a>
+                        </li>
+                        <li><a href="#">Something else here</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a>
+                        </li>
+                      </ul>
+                    </div>-->
 
+
+                    <br /><br />
                     <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
 
-                      <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">First Name <span class="required">*</span>
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Codigo:<span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                          <input readonly="readonly" type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12" value="<?=$ExpedienteTecnicoElaboracion[0]->codigo_unico_pi?>">
                         </div>
-                      </div>
+                     </div>
                       <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Last Name <span class="required">*</span>
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Nombre: <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12">
+                          <textarea class="form-control col-md-7 col-xs-12" readonly="readonly" rows="5">
+                          	<?= trim($ExpedienteTecnicoElaboracion[0]->nombre_pi);?>
+                          </textarea>
                         </div>
                       </div>
                       <div class="form-group">
-                        <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Middle Name / Initial</label>
+                        <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Unidad Ejecutora</label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
+                          <input readonly="readonly" type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12" value="<?=$ExpedienteTecnicoElaboracion[0]->nombre_ue?>">
                         </div>
                       </div>
                       <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Gender</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <div id="gender" class="btn-group" data-toggle="buttons">
-                            <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-                              <input type="radio" name="gender" value="male"> &nbsp; Male &nbsp;
-                            </label>
-                            <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-                              <input type="radio" name="gender" value="female"> Female
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Date Of Birth <span class="required">*</span>
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Costo Total del Proyecto PreInversion <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input id="birthday" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
+                          <input readonly="readonly" type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12" value="<?=$ExpedienteTecnicoElaboracion[0]->costo_total_preinv_et?>">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Costo Total del Proyecto Inversion <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input readonly="readonly" type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12" value="<?=$ExpedienteTecnicoElaboracion[0]->costo_total_inv_et?>">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Tiempo de Ejecucion<span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input readonly="readonly" type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12" value="<?=$ExpedienteTecnicoElaboracion[0]->tiempo_ejecucion_pi_et?>">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Numero de Beneficiarios<span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input readonly="readonly" type="text" id="last-name" name="last-name" required="required" class="form-control col-md-7 col-xs-12" value="<?=$ExpedienteTecnicoElaboracion[0]->num_beneficiarios?>">
                         </div>
                       </div>
                       <div class="ln_solid"></div>
-                      <div class="form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                          <button type="submit" class="btn btn-primary">Cancel</button>
-                          <button type="submit" class="btn btn-success">Submit</button>
-                        </div>
-                      </div>
 
                     </form>
-                  </div>
+                    
+
+
+                    </div>
+
+                 
                 </div>
 			</div>
 		</div>
