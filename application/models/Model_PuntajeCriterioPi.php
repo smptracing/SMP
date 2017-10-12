3b2ef1d2b8f2e38e98658a452eff79a62dd879e1
michael101136 <michael101136@gmail.com>
2017-10-12 10:24:35 -0500

    puntaje criterios
    puntaje criterios

--- application/models/Model_PuntajeCriterioPi.php
+++ application/models/Model_PuntajeCriterioPi.php
@@ -10,7 +10,13 @@ class Model_PuntajeCriterioPi extends CI_Model
 
     function PipPriorizar($funcion)
     {
-        $data=$this->db->query("select * from PROYECTO_INVERSION LEFT JOIN PUNTAJE_CRITERIO_PI ON PROYECTO_INVERSION.id_pi=PUNTAJE_CRITERIO_PI.id_pi LEFT JOIN CRITERIO_ESP on PUNTAJE_CRITERIO_PI.id_criterio=CRITERIO_ESP.id_criterio LEFT JOIN GRUPO_FUNCIONAL ON PROYECTO_INVERSION.id_grupo_funcional=GRUPO_FUNCIONAL.id_grup_funcional left join DIVISION_FUNCIONAL on GRUPO_FUNCIONAL.id_div_funcional=DIVISION_FUNCIONAL.id_div_funcional left join FUNCION on DIVISION_FUNCIONAL.id_funcion=FUNCION.id_funcion where FUNCION.id_funcion='".$funcion."'");
+        $data=$this->db->query("select PRV.id_pi,PRV.codigo_unico_pi,PRV.nombre_pi,PRV.costo_pi,Pc.puntaje_criterio,FUNCION.id_funcion,FUNCION.nombre_funcion from PROYECTO_INVERSION PRV  LEFT JOIN PUNTAJE_CRITERIO_PI Pc ON 
+                                PRV.id_pi=Pc.id_pi LEFT JOIN CRITERIO_ESP on
+                                Pc.id_criterio=CRITERIO_ESP.id_criterio LEFT JOIN GRUPO_FUNCIONAL 
+                                ON PRV.id_grupo_funcional=GRUPO_FUNCIONAL.id_grup_funcional left join 
+                                DIVISION_FUNCIONAL on
+                                GRUPO_FUNCIONAL.id_div_funcional=DIVISION_FUNCIONAL.id_div_funcional left join FUNCION on
+                                DIVISION_FUNCIONAL.id_funcion=FUNCION.id_funcion   where FUNCION.id_funcion='".$funcion."'");
 
         return $data->result();
     }
@@ -19,6 +25,12 @@ class Model_PuntajeCriterioPi extends CI_Model
     	$data=$this->db->query("select * from FUNCION");
 
         return $data->result();
+    }
+    function proyectoInvUnico($id_pi)
+    {
+        $data=$this->db->query("select proI.id_pi,proI.codigo_unico_pi,proI.nombre_pi  from PROYECTO_INVERSION proI where id_pi='".$id_pi."' ");
+
+        return $data->result()[0];
     }
 
 }
