78e0e1413addcb3eb586265b9d696c3c3d70bf95
hamely <sarmiento101147@gmail.com>
2017-10-12 09:47:24 -0500

    .
    .

--- application/views/front/Pmi/PuntajeCriterioPi/insertar.php
+++ application/views/front/Pmi/PuntajeCriterioPi/insertar.php
@@ -1,10 +1,19 @@
 <form class="form-horizontal"  id="form-addAsignarPuntajePi">
 		<div class="row">
 			<div class="col-md-12 col-sm-3 col-xs-12">
+				<label class="control-label">Nombre del Proyecto de Inversion</label>
+				<div>
+					<input id="txtIdPi" name="txtIdPi" value="" class="form-control col-md-4 col-xs-12"  placeholder="Nombre del proyecto" autocomplete="off"  type="hidden">	
+					<input id="txtNombreUe" name="txtNombrePi" value="" class="form-control col-md-4 col-xs-12"  autocomplete="off" readonly="readonly">	
+				</div>	
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 col-sm-3 col-xs-12">
 				<label class="control-label">Seleccionar criterio general</label>
 				<div>
 				
-					<select  id="combofuncion" name="combofuncion" class="form-control col-md-2 col-xs-2">
+					<select  id="combocriteriogeneral" name="combocriteriogeneral" class="form-control col-md-2 col-xs-2">
 							
 								<option value=""></option>
 						
@@ -17,7 +26,7 @@
 				<label class="control-label">Seleccionar criterio especifico</label>
 				<div>
 				
-					<select  id="combofuncion" name="combofuncion" class="form-control col-md-2 col-xs-2">
+					<select  id="combocriterioespecif" name="combocriterioespecif" class="form-control col-md-2 col-xs-2">
 							
 								<option value=""></option>
 						
