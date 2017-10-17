6ba7ce162e3e5971f1b196f728ccb60373927623
hamely <sarmiento101147@gmail.com>
2017-10-17 14:44:02 -0500

    Se modifico error en el popup de reporte orden y se cambio de datatable
    Se modifico error en el popup de reporte orden y se cambio de datatable

--- application/views/front/Reporte/ProyectoInversion/DetalleConcepto.php
+++ application/views/front/Reporte/ProyectoInversion/DetalleConcepto.php
@@ -1,3 +1,8 @@
+ <link href="<?php echo base_url(); ?>assets/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
 <style>
     .modal-dialog
     {
@@ -15,18 +20,15 @@
     }
 </style>
 
+  
 
-
-<script src="<?php echo base_url(); ?>assets/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+<!--<script src="<?php echo base_url(); ?>assets/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
 
 <link href="<?php echo base_url(); ?>assets/vendors/datatables/jquery.dataTables.min.css" rel="stylesheet">
 
-<link href="<?php echo base_url(); ?>assets/vendors/datatables/responsive.dataTables.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>assets/vendors/datatables/responsive.dataTables.min.css" rel="stylesheet">-->
 
-
-    
-    
 
 <div class="right_col" role="main">
 	<div class="">
@@ -68,115 +70,106 @@
 									<br>
 
 									<div class="row">
-						                <div class="col-md-12 col-sm-12 col-xs-12">
-										
-					                        <!--<div class="table-responsive" style="height:300px;overflow:scroll;overflow-x: hidden; ">-->
-                                             
-					                        	<table id="example" class="display nowrap" cellspacing="0" width="100%">
-        <thead>
-            <tr>
-                <th style="width: 2px"></th>
-                <th style="width: 2px">Exp Siaf</th>
-                <th style="width: 2px">Exp Siga</th>
-                <th style="width: 2px">N° Orden</th>
-                  <th>Concepto</th>
-                <th>Tipo bien</th>
-                <th style="width: 3px">Fecha de orden</th>
-                <th>Doc referencia</th>
-                
-              
-                <th>Sub total S/</th>
-                <td>SUB TOTAL SOLES</td>
-                <td>TOTAL IGV SOLES</td>
-                <td>TOTAL FACT SOLES</td>
-                <td>PROVEEDOR</td>
-                <td>DIRECCION</td>
-                <td>GIRO GENERAL</td>
-                <td>NRO RUC</td>
-                <td>TELEFONOS</td>
-                <td>CCI</td>
-                <td>TELEFONO FAX</td>
-                <td>ANO EJE</td>
-                <td>TIPO PPTO</td>
+						                <div class="col-md-12 col-sm-12 col-xs-12" >
+										<div class="table-responsive" style="height:300px;overflow:scroll;overflow-x: hidden; ">
+                                         <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                                          <thead>
+                                            <tr>
+                                                <th></th>
+                                                <th>Exp Siaf</th>
+                                                <th>Exp Siga</th>
+                                                <th>N° Orden</th>
+                                                <th>Tipo bien</th>
+                                                <th>Fecha de orden</th>
+                                                <th>Doc referencia</th>
+                                                <th>Concepto</th>
+                                                <th>Sub total S/.</th>
+                                                <th>Total Igv S/.</th>
+                                                <th>Total Fact S/.</th>
+                                                <th>Proveedor</th>
+                                                <th>Dirección</th>
+                                                <th>Giro General</th>
+                                                <th>Nro Ruc</th>
+                                                <th>Teléfonos</th>
+                                                <th>CCI</th>
+                                                <th>Teléfono Fax</th>
+                                                <th>Año Eje</th>
+                                                <th>Tipo Ppto</th>
+                                            </tr>
+                                          </thead>
+                                          <tbody>
+                                            <?php foreach($listaPorOrden as $item ){ ?>
+                                                        <tr>
+                                                            <td></td>
+                                                            <td>    
+                                                            <button type="button" class="DetalleOrdenExpeSiaf btn btn-primary btn-xs" onclick="detalleordenexpsiaf(<?= (int)$item->ANO_EJE?>,<?= (int)$item->EXP_SIAF?>);"><?=$item->EXP_SIAF?><i class='ace-icon bigger-120'></i></button>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->EXP_SIGA?>
+                                                            </td>
+                                                            <td>
+                                                                <button type="button" class="DetalleOrdenExpeSiaf btn btn-success btn-xs" onclick="detalleporcadanumorden(<?= (int)$item->ANO_EJE?>,'<?=$item->TIPO_BIEN?>',<?=(int)$item->NRO_ORDEN?>,<?=(int)$item->TIPO_PPTO?>);"><?=$item->NRO_ORDEN?><i class='ace-icon bigger-120'></i></button>      
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->TIPO_BIEN?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->FECHA_ORDEN?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->DOCUM_REFERENCIA?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->CONCEPTO?>
+                                                            </td>
+                                                            <td style="text-align:right">
+                                                                <?=number_format($item->SUBTOTAL_SOLES,2)?>
+                                                            </td>
+                                                            <td style="text-align:right">
+                                                                <?=number_format($item->SUBTOTAL_SOLES,2)?>
+                                                            </td>
+                                                            <td style="text-align:right">
+                                                                <?=number_format($item->TOTAL_FACT_SOLES,2)?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->NOMBRE_PROV?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->DIRECCION?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->GIRO_GENERAL?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->NRO_RUC?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->TELEFONOS?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->CCI?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->TELEFONO_FAX?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->ANO_EJE?>
+                                                            </td>
+                                                            <td>
+                                                                <?=$item->TIPO_PPTO?>
+                                                            </td>
 
-                                            
-            </tr>
-        </thead>
-       
-        <tbody>
-           
-                    <?php foreach($listaPorOrden as $item ){ ?>
-                        <tr>
-                            <td></td>
-                            <td>    
-                            <button type="button" class="DetalleOrdenExpeSiaf btn btn-primary btn-xs" onclick="detalleordenexpsiaf(<?= (int)$item->ANO_EJE?>,<?= (int)$item->EXP_SIAF?>);"><?=$item->EXP_SIAF?><i class='ace-icon bigger-120'></i></button>
-                            </td>
-                            <td>
-                                <?=$item->EXP_SIGA?>
-                            </td>
-                            <td>
-                                <button type="button" class="DetalleOrdenExpeSiaf btn btn-success btn-xs" onclick="detalleporcadanumorden(<?= (int)$item->ANO_EJE?>,'<?=$item->TIPO_BIEN?>',<?=(int)$item->NRO_ORDEN?>,<?=(int)$item->TIPO_PPTO?>);"><?=$item->NRO_ORDEN?><i class='ace-icon bigger-120'></i></button>      
-                            </td>
-                             <td>
-                                <?=$item->CONCEPTO?>
-                            </td>
-                            <td>
-                                <?=$item->TIPO_BIEN?>
-                            </td>
-                            <td>
-                                <?=$item->FECHA_ORDEN?>
-                            </td>
-                            <td>
-                                <?=$item->DOCUM_REFERENCIA?>
-                            </td>
-                            
-                           
-                            <td style="text-align:right">
-                                <?=number_format($item->SUBTOTAL_SOLES,2)?>
-                            </td>
-                                <td style="text-align:right">
-                                <?=number_format($item->SUBTOTAL_SOLES,2)?>
-                            </td>
-                            <td style="text-align:right">
-                                <?=number_format($item->TOTAL_IGV_SOLES,2)?>
-                            </td>
-                            <td style="text-align:right">
-                                <?=number_format($item->TOTAL_FACT_SOLES,2)?>
-                            </td>
-                            <td>
-                                <?=$item->NOMBRE_PROV?>
-                            </td>
-                            <td>
-                                <?=$item->DIRECCION?>
-                            </td>
-                            <td>
-                                <?=$item->GIRO_GENERAL?>
-                            </td>
-                            <td>
-                                <?=$item->NRO_RUC?>
-                            </td>
-                            <td>
-                                <?=$item->TELEFONOS?>
-                            </td>
-                            <td>
-                                <?=$item->CCI?>
-                            </td>
-                            <td>
-                                <?=$item->TELEFONO_FAX?>
-                            </td>
-                            <td>
-                                <?=$item->ANO_EJE?>
-                            </td>
-                            <td>
-                                <?=$item->TIPO_PPTO?>
-                            </td>
-                          
-                      </tr>
-                    <?php } ?>
-            
-        </tbody>
-    </table>
-</div>
+                                                              </tr>
+                                                    <?php } ?>
+                                          </tbody>
+                                        </table>
+
+                                        </div>
+                                        </div>
+
+					                        <!--<div class="table-responsive" style|="height:300px;overflow:scroll;overflow-x: hidden; ">-->
+                                             
+
                                                 <!--<table id="dynamic-table"  class="table table-striped jambo_table bulk_action  table-hover" cellspacing="0" width="2500px">
 												<thead>
 												<tr>
@@ -203,6 +196,7 @@
 												 </tr>
 												</thead>
 												<body>
+                                                                <?=number_format($item->TOTAL_IGV_SOLES,2)?>
 													<?php foreach($listaPorOrden as $item ){ ?>
 													  	<tr>
 															<td>	
@@ -230,8 +224,7 @@
 																<?=number_format($item->SUBTOTAL_SOLES,2)?>
 													    	</td>
 													    	<td style="text-align:right">
-																<?=number_format($item->TOTAL_IGV_SOLES,2)?>
-													    	</td>
+                                                            </td>
 													    	<td style="text-align:right">
 																<?=number_format($item->TOTAL_FACT_SOLES,2)?>
 													    	</td>
@@ -281,14 +274,6 @@
 							
 
 
-                    						
-
-
-
-
-
-
- 
 
 								</div>	
 							</div>
@@ -302,7 +287,7 @@
 </div>
 
 <script>
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('#example').DataTable( {
         "language":idioma_espanol,
         "searching": true,
@@ -320,7 +305,7 @@ $(document).ready(function() {
         } ],
         order: [ 1, 'asc' ]
     } );
-} );
+} );*/
 /*$(document).ready(function()
 	{
 		
@@ -372,3 +357,20 @@ function detalleporcadanumorden(anio,tipobien,numorden,tipoppto)
 
 
 </script>
+  <!-- Custom Theme Scripts -->
+    <script src="<?php echo base_url(); ?>assets/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url(); ?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Datatables -->
+
+    <script src="<?php echo base_url(); ?>assets/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+
+    <script src="<?php echo base_url(); ?>assets/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script> <!--PARA EL BOTON-->
+    <script src="<?php echo base_url(); ?>assets/build/js/custom.min.js"></script>
+
+
+
+
+
+
