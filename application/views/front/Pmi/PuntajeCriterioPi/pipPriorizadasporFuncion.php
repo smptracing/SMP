e12c6e5336b0645e695dafe83c114edaae498175
hamely <sarmiento101147@gmail.com>
2017-10-19 09:57:11 -0500

    Aplicativo Movil: Se creo la ruta para el aplicativo movil, el controlador AplicativoMovil y una vista en blanco
    Aplicativo Movil: Se creo la ruta para el aplicativo movil, el
    controlador AplicativoMovil y una vista en blanco

--- application/views/front/Pmi/PuntajeCriterioPi/pipPriorizadasporFuncion.php
+++ application/views/front/Pmi/PuntajeCriterioPi/pipPriorizadasporFuncion.php
@@ -46,7 +46,13 @@
 										
 
 										<div class="col-md-12 col-xs-12">
-						
+											
+											<div class="clearfix">
+												<div class="pull-right tableTools-container"></div>
+											</div>
+
+										<br>
+
 											<div class="x_content">
 												<table id="table-pippriorizadasporfuncion" class="table table-striped jambo_table bulk_action  table-hover" cellspacing="0" width="100%">
 													<thead>
@@ -106,10 +112,41 @@
 		var valor=$("#Aniao").val();
 		$("#comboanio option[value="+valor+"]").attr("selected", true);
 
-		$('#table-pippriorizadasporfuncion').DataTable(
+
+		var myTable=$('#table-pippriorizadasporfuncion').DataTable(
 		{
-			"language" : idioma_espanol
+			"language":idioma_espanol,
+            "searching": true,
+             "info":     true,
+            "paging":   true,
 		});
+
+
+			$.fn.dataTable.Buttons.defaults.dom.container.className = 'dt-buttons btn-overlap btn-group btn-overlap';
+				
+				new $.fn.dataTable.Buttons( myTable, {
+					buttons: [
+					  {
+						"extend": "excel",
+						"text": "<i class='fa fa-file-excel-o bigger-110 green'></i> <span class='hidden'>Export to Excel</span>",
+						"className": "btn btn-white btn-primary btn-bold"
+					  },
+					  {
+						"extend": "pdf",
+						"text": "<i class='fa fa-file-pdf-o bigger-110 red'></i> <span class='hidden'>Export to PDF</span>",
+						"className": "btn btn-white btn-primary btn-bold"
+					  },
+					  {
+						"extend": "print",
+						"text": "<i class='fa fa-print bigger-110 grey'></i> <span class='hidden'>Print</span>",
+						"className": "btn btn-white btn-primary btn-bold",
+						autoPrint: false,
+						message: 'This print was produced using the Print button for DataTables'
+					  }		  
+					]
+				} );
+				myTable.buttons().container().appendTo( $('.tableTools-container') );
+
 
 		$('#combofuncion').change('click', function(e)
 		{
