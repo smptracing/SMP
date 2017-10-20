a262b98c41d731b38c8d14b27f4926b7d6a44175
michael101136 <michael101136@gmail.com>
2017-10-20 07:53:57 -0500

    limpiando codigo en el aplicativo web
    limpiando codigo en el aplicativo web

--- application/views/front/Aplicativo_Movil/index.php
+++ application/views/front/Aplicativo_Movil/index.php
@@ -291,62 +291,11 @@ var base_url = '<?php echo base_url(); ?>';
         <!-- /.col -->
       </div>
 
-
-       
-          <!-- /.info-box 
-          <div class="info-box bg-green">
-            <span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">NO PIP</span>
-              <span class="info-box-number">92,050</span>
-				
-              <div class="progress">
-                <div class="progress-bar" style="width: 20%"></div>
-              </div>
-              <span class="progress-description">
-                    20% Increase in 30 Days
-                  </span>
-            </div>
-<<<<<<< HEAD
-            
-
-          </div>
-
-          <div class="info-box bg-red">
-            <span class="info-box-icon"><i class="ion ion-ios-cloud-download-outline"></i></span>
-=======
->>>>>>> origin/desarrollo
-
-          </div>-->
-
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-              </div>
-              <span class="progress-description">
-                    70% Increase in 30 Days
-                  </span>
             </div>
 
           </div>
 
-          <div class="info-box bg-aqua">
-            <span class="info-box-icon"><i class="ion-ios-chatbubble-outline"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Direct Messages</span>
-              <span class="info-box-number">163,921</span>
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 40%"></div>
-              </div>
-              <span class="progress-description">
-                    40% Increase in 30 Days
-                  </span>
-            </div>
-
-          </div>
+         
 
         </div>
 
@@ -451,6 +400,7 @@ var base_url = '<?php echo base_url(); ?>';
 		        success: function(marcadores)
 		        {
 
+		        	 console.log(marcadores);
 		        	 var marcadores=JSON.parse(marcadores);
 
 				      for (i = 0; i < marcadores.length; i++) {  
