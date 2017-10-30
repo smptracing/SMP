04b0c9374cd5d87fd9cde6a524833123a89f0e02
michael101136 <michael101136@gmail.com>
2017-10-30 09:03:48 -0500

    ordenamiento
    ordenamiento

--- application/models/Model_ET_Presupuesto_Analitico.php
+++ application/models/Model_ET_Presupuesto_Analitico.php
@@ -24,7 +24,7 @@ class Model_ET_Presupuesto_Analitico extends CI_Model
 	}
 	function ETPresupuestoAnaliticoPorIdET($idET)
 	{
-		$data=$this->db->query("select * from ET_PRESUPUESTO_ANALITICO as ETPA inner join ET_CLASIFICADOR as ETC on ETPA.id_clasificador=ETC.id_clasificador inner join ET_PRESUPUESTO_EJECUCION as ETPE on ETPA.id_presupuesto_ej=ETPE.id_presupuesto_ej where ETPA.id_eT=".$idET);
+		$data=$this->db->query("select * from ET_PRESUPUESTO_ANALITICO as ETPA inner join ET_CLASIFICADOR as ETC on ETPA.id_clasificador=ETC.id_clasificador inner join ET_PRESUPUESTO_EJECUCION as ETPE on ETPA.id_presupuesto_ej=ETPE.id_presupuesto_ej where ETPA.id_eT=".$idET." order by desc_presupuesto_ej ");
 
 		return $data->result();
 	}
