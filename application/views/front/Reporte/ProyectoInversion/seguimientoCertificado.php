febac6c2773b5ae350707f7a03b82c7e926e2137
Manuel\MEL <raullivano@gmail.com>
2017-11-09 17:03:25 -0500

    agregando contenedor a reporte

--- application/views/front/Reporte/ProyectoInversion/seguimientoCertificado.php
+++ application/views/front/Reporte/ProyectoInversion/seguimientoCertificado.php
@@ -12,7 +12,8 @@
 
 									<div class="row">  
 										<div class="col-md-12 col-sm-12 col-xs-12">
-											
+											<div class="x_panel">
+													<div class="container">
 														BÚSQUEDA POR AÑO: 
 														<div class="row">
 													
@@ -102,7 +103,8 @@
 																					<?php } ?>
 																				</tbody>	
 																		  </table> 
-														
+															</div>
+														</div>
 										</div>
 									</div>
 										<!-- / fin tabla de sector desde el row -->
