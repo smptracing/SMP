2ece0884845952027287b7a5379046a3fe409280
nyxjoel <nyxjoel@gmail.com>
2017-12-07 15:27:58 -0500

    #52 Tooltip Criterios de Priorizacion - Fixed

--- application/views/front/Pmi/CriteriosGenerales/criteriosFuncion.php
+++ application/views/front/Pmi/CriteriosGenerales/criteriosFuncion.php
@@ -73,37 +73,37 @@
 					                    </span>
 					                </div>
 			            		</div>
-							</div>						
+							</div>
 							<div class="form-group">
 				                <label class="control-label" for="inputGroup">Reportes </label>
 				                <div class="input-group">
 									<div class="pull-right tableTools-container ">&nbsp;&nbsp;</div>
 				                </div>
 			            	</div>
-						</div>	
+						</div>
 						<div class="table-responsive">
 							<table id="table-Presupuesto" class="table table-striped jambo_table bulk_action  table-hover" cellspacing="0" width="100%">
 								<thead>
 									<tr>
 										<td style="width: 1%">Codigo</td>
 										<td>Función</td>
-										
+
 										<td style="width: 1%">Opciones</td>
 									</tr>
 								</thead>
 								<tbody>
 										<?php foreach($listaCriterioFuncion as $item ){ ?>
 								  	<tr>
-										
+
 										<td>
 											<?=$item->codigo_funcion?>
 								    	</td>
 										<td>
 											<?=$item->nombre_funcion?>
 								    	</td>
-								    
+
 								    	<td style="text-align: left;">
-								    		
+
 											<button type="button" class="btn btn-info btn-xs " data-placement="top" data-toggle="tooltip" data-original-title="Agregar criterios Generales" onclick="paginaAjaxDialogo(null, 'Registro Criterio Generales', { id_funcion:'<?=$item->id_funcion?>', nombre_funcion:'<?=$item->nombre_funcion?>',anio:'<?= $anio;?>' }, base_url+'index.php/PmiCriterioG/insertar', 'GET', null, null, false, true);"><span class="fa fa-th-list"></span>
 											</button>
 											<button type="button" class="btn btn-success btn-xs " data-placement="top" data-toggle="tooltip" data-original-title="Reporte De Criterios Generales y Especificos" onclick="ReporteCriteriosGenerales(<?=$item->id_funcion?>,<?= $anio;?>);"><i class="fa fa-file-pdf-o"></i>
@@ -112,9 +112,9 @@
 								  </tr>
 								<?php } ?>
 								</tbody>
-								
+
 							</table>
-							
+
 						</div>
 						<!--<div class="" role="tabpanel" data-example-id="togglable-tabs">
 							<ul id="myTab" class="nav nav-tabs" role="tablist">
@@ -138,7 +138,7 @@
 										                </div>
 								            		</div>
 												</div>
-											
+
 												<div class="form-group">
 										                <label class="control-label" for="inputGroup">Reportes </label>
 										                <div class="input-group">
@@ -146,30 +146,30 @@
 
 										                </div>
 								            	</div>
-											</div>			
+											</div>
 											<div class="x_content">
 												<table id="table-Presupuesto" class="table table-striped jambo_table bulk_action  table-hover" cellspacing="0" width="100%">
 													<thead>
 														<tr>
 															<td style="width: 1%">Codigo</td>
 															<td>Función</td>
-															
+
 															<td style="width: 1%">Opciones</td>
 														</tr>
 													</thead>
 													<tbody>
 															<?php foreach($listaCriterioFuncion as $item ){ ?>
 													  	<tr>
-															
+
 															<td>
 																<?=$item->codigo_funcion?>
 													    	</td>
 															<td>
 																<?=$item->nombre_funcion?>
 													    	</td>
-													    
+
 													    	<td style="text-align: left;">
-													    		
+
 																<button type="button" class="btn btn-info btn-xs " data-placement="top" data-toggle="tooltip" data-original-title="Agregar criterios Generales" onclick="paginaAjaxDialogo(null, 'Registro Criterio Generales', { id_funcion:'<?=$item->id_funcion?>', nombre_funcion:'<?=$item->nombre_funcion?>',anio:'<?= $anio;?>' }, base_url+'index.php/PmiCriterioG/insertar', 'GET', null, null, false, true);"><span class="fa fa-th-list"></span>
 																</button>
 																<button type="button" class="btn btn-success btn-xs " data-placement="top" data-toggle="tooltip" data-original-title="Reporte De Criterios Generales y Especificos" onclick="ReporteCriteriosGenerales(<?=$item->id_funcion?>,<?= $anio;?>);"><i class="fa fa-file-pdf-o"></i>
@@ -178,7 +178,7 @@
 													  </tr>
 													<?php } ?>
 													</tbody>
-													
+
 												</table>
 											</div>
 										</div>
@@ -199,10 +199,10 @@
 	{
 		var myTable=$('#table-Presupuesto').DataTable(
 		{
-			
+
 			"language" : idioma_espanol,
 
-				
+
 		});
 
 		$.fn.dataTable.Buttons.defaults.dom.container.className = 'dt-buttons btn-overlap btn-group btn-overlap';
@@ -211,23 +211,23 @@
 			buttons: [
 			  {
 				"extend": "excel",
-				"text": "<i class='fa fa-file-excel-o bigger-110 green'></i> <span class='hidden'>Export to Excel</span>",
+				"text": "<i class='fa fa-file-excel-o bigger-110 green'></i> <span class='' data-placement='top' data-toggle='tooltip' data-original-title='Exportar a Excel'>Excel</span>",
 				"className": "btn btn-white btn-primary btn-bold"
 			  },
 			  {
 				"extend": "pdf",
-				"text": "<i class='fa fa-file-pdf-o bigger-110 red'></i> <span class='hidden'>Export to PDF</span>",
+				"text": "<i class='fa fa-file-pdf-o bigger-110 red'></i> <span class='' data-placement='top' data-toggle='tooltip' data-original-title='Exportar a PDF'>PDF</span>",
 				"className": "btn btn-white btn-primary btn-bold",
 				"pageSize": 'LEGAL',
 				orientation: 'landscape',
 			  },
 			  {
 				"extend": "print",
-				"text": "<i class='fa fa-print bigger-110 grey'></i> <span class='hidden'>Print</span>",
+				"text": "<i class='fa fa-print bigger-110 grey'></i> <span class='' data-placement='top' data-toggle='tooltip' data-original-title='Imprimir'>Imprimir</span>",
 				"className": "btn btn-white btn-primary btn-bold",
 				autoPrint: false,
 				message: 'This print was produced using the Print button for DataTables'
-			  }		  
+			  }
 			]
 		} );
 		myTable.buttons().container().appendTo( $('.tableTools-container') );
@@ -240,15 +240,15 @@
 				window.location.href=base_url+"index.php/PmiCriterioG/criterioFuncion/"+anio;
 			}
 		});
-		
 
-		
+
+
 
 	});
 
 	function ReporteCriteriosGenerales(id_funcion,anio_criterio_gen)
 		{
-				
+
 				/*var parametros = {
                 "anio_criterio_gen" : anio_criterio_gen,
                 "id_funcion" : id_funcion
@@ -266,7 +266,7 @@
 		        });*/
 		    	var urll=base_url+"index.php/PmiCriterioG/ReporteCriteriosG/"+id_funcion+'.'+anio_criterio_gen;
 		        ventana=window.open(urll, 'Nombre de la ventana', 'width=1400,height=800');
-					
+
 		}
 
 </script>
