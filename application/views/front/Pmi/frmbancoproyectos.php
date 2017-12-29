50631680e8bbe5dabea3c20a4f794401872635d8
xpatrickt <patrickrsq@gmail.com>
2017-12-29 10:55:12 -0500

    Editar Banco de inversiones

--- application/views/front/Pmi/frmbancoproyectos.php
+++ application/views/front/Pmi/frmbancoproyectos.php
@@ -857,6 +857,7 @@
                                          </div> -->
                                 </div>
                             </div>
+                        </div>
                             <div class="row">
                                 <div class="col-md-4">
                                     <!-- <div class="col-md-6 col-sm-6 col-xs-12">
