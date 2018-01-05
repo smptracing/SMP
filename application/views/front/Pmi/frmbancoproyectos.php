f50995dba90c4bf5d8e8bdb93ff12ad3ad3646f1
nyxjoel <nyxjoel@gmail.com>
2018-01-05 12:47:44 -0500

    Hide Button - Estado

--- application/views/front/Pmi/frmbancoproyectos.php
+++ application/views/front/Pmi/frmbancoproyectos.php
@@ -252,14 +252,14 @@
                         </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="col-md-6 col-sm-6 col-xs-12">
+                            <!-- <div class="col-md-6 col-sm-6 col-xs-12">
                                 <label  for="name">Estado<span class="required"></span>
                                 </label>
                                 <select id="cbx_estado_pi" name="cbx_estado_pi" class="selectpicker">
                                   <option value='1'>Activo</option>
                                   <option value='0'>Inactivo</option>
                                 </select>
-                            </div>
+                            </div> -->
                         </div>
                         </div>
 
